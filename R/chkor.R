b4b0f122ba1af636763590c1a1832bb31ab25411
joethorley <joe@poissonconsulting.ca>
2019-08-01 11:55:37 -0700

    err.R to messages.R

--- R/chkor.R
+++ R/chkor.R
@@ -1,3 +1,13 @@
+try_chk <- function (expr) {
+  try(eval(expr, envir = parent.frame(3)), silent = TRUE)
+}
+
+try_msg <- function (x) {
+    x <- as.character(x)
+    x <- sub("^Error.*[:] ", "", x, perl = TRUE)
+    sub("\n$", "", x, perl = TRUE)
+}
+
 #' Check OR
 #'
 #' @param ... Multiple \code{\link{chk}} functions.
--- R/internal.R
+++
@@ -1,9 +0,0 @@
-try_chk <- function (expr) {
-  try(eval(expr, envir = parent.frame(3)), silent = TRUE)
-}
-
-try_msg <- function (x) {
-    x <- as.character(x)
-    x <- sub("^Error.*[:] ", "", x, perl = TRUE)
-    sub("\n$", "", x, perl = TRUE)
-}
