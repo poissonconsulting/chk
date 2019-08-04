e140f5d168dd1e02012d95c82ec38e7e95c4300f
joethorley <joe@poissonconsulting.ca>
2019-08-04 11:05:44 -0700

    chk_dim

--- R/chk-standalone.R
+++ R/chk-standalone.R
@@ -99,18 +99,6 @@ chk_null <- function(x, err = TRUE) {
   stop("`", x_name, "` must be NULL.", call. = FALSE)
 }
 
-#' @describeIn chk_flag Check Function
-#' @export
-#
-#  Licence: CC
-#  Repository: https://github.com/poissonconsulting/chk
-chk_function <- function(x, err = TRUE) {
-  if(is.function(x)) return(TRUE)
-  if(!err) return(FALSE)
-  x_name <- deparse(substitute(x))
-  stop("`", x_name, "` must be a function.", call. = FALSE)
-}
-
 #' @describeIn chk_flag Check Inherits from Class
 #' @export
 #
@@ -126,6 +114,24 @@ chk_is <- function(x, class, err = TRUE) {
        x_class, "'.", call. = FALSE)
 }
 
+#' @describeIn chk_flag Check Dim
+#' @param dim An integer vector (or object that can be converted to an integer vector)
+#'  of the dimensions of x.
+#' @export
+#
+#  Licence: CC
+#  Repository: https://github.com/poissonconsulting/chk
+chk_dim <- function(x, dim, err = TRUE) {
+  dim <- as.integer(dim)
+  if(identical(dim(x), as.integer(dim))) return(TRUE)
+  if(!err) return(FALSE)
+  x_name <- deparse(substitute(x))
+  dim <- paste0(dim, collapse = " ")
+  x_dim <- paste(dim(x), collapse = " ")
+  stop("`", x_name, "` dim must be '", dim, "', not '", 
+       x_dim, "'.", call. = FALSE)
+}
+
 #' @describeIn chk_flag Check No Missing Values
 #' 
 #' @export
@@ -137,6 +143,18 @@ chk_no_missing <- function(x, err = TRUE) {
   if(!err) return(FALSE)
   x_name <- deparse(substitute(x))
   stop("`", x_name, "` must not have missing values.", call. = FALSE)
+}
+
+#' @describeIn chk_flag Check Function
+#' @export
+#
+#  Licence: CC
+#  Repository: https://github.com/poissonconsulting/chk
+chk_function <- function(x, err = TRUE) {
+  if(is.function(x)) return(TRUE)
+  if(!err) return(FALSE)
+  x_name <- deparse(substitute(x))
+  stop("`", x_name, "` must be a function.", call. = FALSE)
 }
 
 #' @describeIn chk_flag Check ... Unused
