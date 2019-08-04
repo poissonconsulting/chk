2ef9a8969a085a84ca7647d4c664f2423b9966d0
joethorley <joe@poissonconsulting.ca>
2019-08-04 09:25:47 -0700

    basic chk_values

--- R/chk-values.R
+++ R/chk-values.R
@@ -1,12 +1,44 @@
 #' @describeIn chk_flag Check Values
-#' Missing values are ignored
+#'
+#' Checks if all x are in values.
 #' @param values A vector of the permitted value(s). If
-#' two values then it is considered to be a value range.
+#' two non-missing values then it is considered to be a value range.
 #' @export
 #
 #  Licence: CC
 #  Repository: https://github.com/poissonconsulting/chk
 #  depends on R/cc.R
 chk_values <- function (x, values = c(-Inf, Inf, NA), err = TRUE) {
-  return(TRUE)
+  if(all(x %in% values)) return(TRUE)
+  values_not_na <- values[!is.na(values)]
+  bracket <- if(is.character(x)) "'" else ""
+  if(length(values_not_na) != 2L) {
+    if(!err) return(FALSE)
+    if(length(values_not_na) != length(values)) 
+      values <- c(values_not_na, NA)
+    x_name <- deparse(substitute(x))
+    if(length(x) == 1) {
+      stop("`", x_name, "` must be ", cc(values, bracket = bracket), 
+           ", not ", cc(x, bracket = bracket), ".", call. = FALSE)
+    }
+    stop("All values of `", x_name, "` must be ", cc(values, bracket = bracket), 
+         ".", call. = FALSE)
+  }
+  x_not_na <- x[!is.na(x)]
+  if(!all(x_not_na >= min(values_not_na) & x_not_na <= max(values_not_na))) {
+    if(!err) return(FALSE)
+    x_name <- deparse(substitute(x))
+    if(length(x) == 1) {
+      stop("`", x_name, "` must be ", cc(values, con = " to ", bracket = bracket), 
+           ", not ", cc(x, bracket = bracket), ".", call. = FALSE)
+    }
+    stop("All values of `", x_name, "` must be ", cc(values, con = " to ", bracket = bracket), 
+         ".", call. = FALSE)
+  }
+  if(length(x_not_na) != length(x) && length(values_not_na) == length(values)) {
+    if(!err) return(FALSE)
+    x_name <- deparse(substitute(x))
+    stop("`", x_name, "` must not have missing values.", call. = FALSE)
+  }
+  TRUE
 }
---
+++ tests/testthat/test-chk-values.R
@@ -0,0 +1,10 @@
+context("chk-values")
+
+test_that("chk_values", {
+  expect_true(chk_values(1))
+  expect_true(chk_values(1:3))
+  expect_true(chk_values(numeric(0), 1L))
+  expect_error(chk_values(NA, 0L), c("`NA` must be 0, not NA"))
+  expect_error(chk_values(1L, 0L), c("`1L` must be 0, not 1"))
+#  expect_error(chk_values(1, ""), "`1` must be '', not '1'")
+})
