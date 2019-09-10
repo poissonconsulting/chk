c5412609d78d51d2444e2403f6ca6abc0f7f079b
joethorley <joe@poissonconsulting.ca>
2019-09-10 15:45:22 -0700

    vld_range

--- R/chk-range.R
+++ R/chk-range.R
@@ -1,40 +1,30 @@
-#' Check Range
+#' Check/Validate Range
 #' 
-#' Checks range of non-missing values.
+#' Checks/validates range of non-missing values.
 #'
-#' @details  
-#' 
-#' \code{chk_range()}: Check Range
-#' 
-#' Checks if all non-missing values fall within range using:
-#' 
-#' \code{all(x[!is.na(x)] >= range[1] & x[!is.na(x)] <= range[2])}
-#' 
-#' Range should be a non-missing sorted vector of length 2.
-#'  
 #' @inheritParams chk_true
-#' @param err Deprecated.
 #' @param value A non-missing scalar of a value.
 #' @param range A vector of length 2 of the lower and upper permitted values.
-#' @return TRUE if passes check. Otherwise if throws an informative error unless
-#' \code{err = FALSE} in which case it returns FALSE.
+#' @return The \code{chk_} functions throw an informative error if the test fails.
+#' The \code{vld_} functions return a flag indicating whether the test was met.
+#' @name chk_range
+NULL
+
+#' @describeIn chk_range Check Range
+#' 
+#' Checks if all non-missing values fall within range using
+#' \code{vld_range()}.
 #' 
 #' @export
 #' 
 #' @examples 
 #' 
 #' # chk_range
-#' chk_range(numeric(0))
 #' chk_range(0)
 #' try(chk_range(-0.1))
-#' chk_range(c(0.1, 0.2, NA))
-#' chk_range(c(0.1, 0.2, NA), range = c(0,1))
-#' 
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_range <- function (x, range = c(0, 1), err = TRUE, x_name = NULL) {
-  if(all(x[!is.na(x)] >= range[1] & x[!is.na(x)] <= range[2])) return(TRUE)
-  if(!err) return(FALSE)
+chk_range <- function (x, range = c(0, 1), x_name = NULL) {
+  if(vld_range(x, range)) return(invisible())
+
   if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
   if(length(x) == 1L) {
     if(range[1] == range[2])
@@ -47,9 +37,52 @@ chk_range <- function (x, range = c(0, 1), err = TRUE, x_name = NULL) {
   stop(x_name, " must have values between ", cc(range, " and "), ".", call. = FALSE)
 }
 
+#' @describeIn chk_range Validate Range
+#' 
+#' Validates all non-missing values fall within range using:
+#' 
+#' \code{all(x[!is.na(x)] >= range[1] & x[!is.na(x)] <= range[2])}
+#' 
+#' Range should be a non-missing sorted vector of length 2.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # vld_range
+#' vld_range(numeric(0))
+#' vld_range(0)
+#' vld_range(-0.1)
+#' vld_range(c(0.1, 0.2, NA))
+#' vld_range(c(0.1, 0.2, NA), range = c(0,1))
+vld_range <- function (x, range = c(0, 1)) {
+  all(x[!is.na(x)] >= range[1] & x[!is.na(x)] <= range[2])
+}
+
+
 #' @describeIn chk_range Check Less Than
 #' 
-#' Checks if all non-missing values are less than value using:
+#' Checks if all non-missing values are less than value using
+#' \code{vld_lt()}.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # chk_lt
+#' chk_lt(-0.1)
+#' try(chk_lt(c(-0.1, 0.2)))
+chk_lt <- function (x, value = 0, x_name = NULL) {
+  if(vld_lt(x, value)) return(invisible())
+  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
+  if(length(x) == 1L)
+    stop(x_name, " must be less than ", cc(value), ", not ", cc(x), ".", call. = FALSE)
+  stop(x_name, " must have values less than ", cc(value), ".", call. = FALSE)
+}
+
+#' @describeIn chk_range Validate Less Than
+#' 
+#' Validates all non-missing values are less than value using
 #' 
 #' \code{all(x[!is.na(x)] < value))}
 #' 
@@ -59,29 +92,41 @@ chk_range <- function (x, range = c(0, 1), err = TRUE, x_name = NULL) {
 #' 
 #' @examples 
 #' 
-#' # chk_lt
-#' chk_lt(numeric(0))
-#' try(chk_lt(0))
-#' chk_lt(-0.1)
-#' chk_lt(c(-0.1, -0.2, NA))
-#' try(chk_lt(c(-0.1, 0.2)))
-#' chk_lt(c(-0.1, 0.2), value = 1)
-#' chk_lt("a", value = "b")
-#'  
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_lt <- function (x, value = 0, err = TRUE, x_name = NULL) {
-  if(all(x[!is.na(x)] < value)) return(TRUE)
-  if(!err) return(FALSE)
-  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
-  if(length(x) == 1L)
-    stop(x_name, " must be less than ", cc(value), ", not ", cc(x), ".", call. = FALSE)
-  stop(x_name, " must have values less than ", cc(value), ".", call. = FALSE)
-}
+#' # vld_lt
+#' vld_lt(numeric(0))
+#' vld_lt(0)
+#' vld_lt(-0.1)
+#' vld_lt(c(-0.1, -0.2, NA))
+#' vld_lt(c(-0.1, 0.2))
+#' vld_lt(c(-0.1, 0.2), value = 1)
+#' vld_lt("a", value = "b")
+vld_lt <- function (x, value = 0) all(x[!is.na(x)] < value)
 
 #' @describeIn chk_range Check Less Than or Equal To
 #' 
-#' Checks if all non-missing values are less than or equal to y using:
+#' Checks if all non-missing values are less than or equal to y using
+#' \code{vld_lte()}.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # chk_lte
+#' chk_lte(0)
+#' try(chk_lte(0.1))
+chk_lte <- function (x, value = 0, x_name = NULL) {
+  if(vld_lte(x, value)) return(invisible())
+  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
+  if(length(x) == 1L)
+    stop(x_name, " must be less than or equal to ", cc(value), ", not ", cc(x), 
+         ".", call. = FALSE)
+  stop(x_name, " must have values less than or equal to ", cc(value), ".", 
+       call. = FALSE)
+}
+
+#' @describeIn chk_range Validate Less Than or Equal To
+#' 
+#' Validates all non-missing values are less than or equal to y using
 #' 
 #' \code{all(x[!is.na(x)] <= value))}
 #' 
@@ -91,29 +136,41 @@ chk_lt <- function (x, value = 0, err = TRUE, x_name = NULL) {
 #' 
 #' @examples 
 #' 
-#' # chk_lte
-#' chk_lte(numeric(0))
-#' chk_lte(0)
-#' try(chk_lte(0.1))
-#' chk_lte(c(-0.1, -0.2, NA))
-#' try(chk_lte(c(-0.1, -0.2, NA), value = -1))
-#'  
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_lte <- function (x, value = 0, err = TRUE, x_name = NULL) {
-  if(all(x[!is.na(x)] <= value)) return(TRUE)
-  if(!err) return(FALSE)
-  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
-  if(length(x) == 1L)
-    stop(x_name, " must be less than or equal to ", cc(value), ", not ", cc(x), 
-         ".", call. = FALSE)
-  stop(x_name, " must have values less than or equal to ", cc(value), ".", 
-       call. = FALSE)
-}
+#' # vld_lte
+#' vld_lte(numeric(0))
+#' vld_lte(0)
+#' vld_lte(0.1)
+#' vld_lte(c(-0.1, -0.2, NA))
+#' vld_lte(c(-0.1, -0.2, NA), value = -1)
+vld_lte <- function (x, value = 0) all(x[!is.na(x)] <= value)
 
 #' @describeIn chk_range Check Greater Than
 #' 
-#' Checks if all non-missing values are greater than value using:
+#' Checks if all non-missing values are greater than value using
+#' \code{vld_gt()}.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # chk_gt
+#' chk_gt(0.1)
+#' try(chk_gt(c(0.1, -0.2)))
+#'  
+#  Licence: CC0
+#  Repository: https://github.com/poissonconsulting/chk
+chk_gt <- function (x, value = 0, x_name = NULL) {
+  if(vld_gt(x, value)) return(invisible())
+  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
+  if(length(x) == 1L)
+    stop(x_name, " must be greater than ", cc(value), ", not ", cc(x), ".", 
+         call. = FALSE)
+  stop(x_name, " must have values greater than ", cc(value), ".", call. = FALSE)
+}
+
+#' @describeIn chk_range Validate Greater Than
+#' 
+#' Validates all non-missing values are greater than value using
 #' 
 #' \code{all(x[!is.na(x)] > value))}
 #' 
@@ -123,30 +180,41 @@ chk_lte <- function (x, value = 0, err = TRUE, x_name = NULL) {
 #' 
 #' @examples 
 #' 
-#' # chk_gt
-#' chk_gt(numeric(0))
-#' try(chk_gt(0))
-#' chk_gt(0.1)
-#' chk_gt(c(0.1, 0.2, NA))
-#' try(chk_gt(c(0.1, -0.2)))
-#' chk_gt(c(-0.1, 0.2), value = -1)
-#' chk_gt("b", value = "a")
-#'  
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_gt <- function (x, value = 0, err = TRUE, x_name = NULL) {
-  if(all(x[!is.na(x)] > value)) return(TRUE)
-  if(!err) return(FALSE)
-  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
-  if(length(x) == 1L)
-    stop(x_name, " must be greater than ", cc(value), ", not ", cc(x), ".", 
-         call. = FALSE)
-  stop(x_name, " must have values greater than ", cc(value), ".", call. = FALSE)
-}
+#' # vld_gt
+#' vld_gt(numeric(0))
+#' vld_gt(0)
+#' vld_gt(0.1)
+#' vld_gt(c(0.1, 0.2, NA))
+#' vld_gt(c(0.1, -0.2))
+#' vld_gt(c(-0.1, 0.2), value = -1)
+#' vld_gt("b", value = "a")
+vld_gt <- function (x, value = 0) all(x[!is.na(x)] > value)
 
 #' @describeIn chk_range Check Greater Than or Equal To
 #' 
-#' Checks if all non-missing values are greater than or equal to y using:
+#' Checks if all non-missing values are greater than or equal to y using 
+#' \code{vld_gte()}.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # chk_gte
+#' chk_gte(0)
+#' try(chk_gte(-0.1))
+chk_gte <- function (x, value = 0, x_name = NULL) {
+  if(vld_gte(x, value)) return(invisible())
+  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
+  if(length(x) == 1L)
+    stop(x_name, " must be greater than or equal to ", cc(value), 
+        ", not ", cc(x), ".", call. = FALSE)
+  stop(x_name, " must have values greater than or equal to ", cc(value), ".", 
+       call. = FALSE)
+}
+
+#' @describeIn chk_range Validate Greater Than or Equal To
+#' 
+#' Validates all non-missing values are greater than or equal to y using:
 #' 
 #' \code{all(x[!is.na(x)] >= value))}
 #' 
@@ -156,22 +224,10 @@ chk_gt <- function (x, value = 0, err = TRUE, x_name = NULL) {
 #' 
 #' @examples 
 #' 
-#' # chk_gte
-#' chk_gte(numeric(0))
-#' chk_gte(0)
-#' try(chk_gte(-0.1))
-#' chk_gte(c(0.1, 0.2, NA))
-#' try(chk_gte(c(0.1, 0.2, NA), value = 1))
-#'  
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_gte <- function (x, value = 0, err = TRUE, x_name = NULL) {
-  if(all(x[!is.na(x)] >= value)) return(TRUE)
-  if(!err) return(FALSE)
-  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
-  if(length(x) == 1L)
-    stop(x_name, " must be greater than or equal to ", cc(value), 
-        ", not ", cc(x), ".", call. = FALSE)
-  stop(x_name, " must have values greater than or equal to ", cc(value), ".", 
-       call. = FALSE)
-}
+#' # vld_gte
+#' vld_gte(numeric(0))
+#' vld_gte(0)
+#' vld_gte(-0.1)
+#' vld_gte(c(0.1, 0.2, NA))
+#' vld_gte(c(0.1, 0.2, NA), value = 1)
+vld_gte <- function (x, value = 0) all(x[!is.na(x)] >= value)
