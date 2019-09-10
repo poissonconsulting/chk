d055a26a203e1adbd6e1c8158167765259fd8835
joethorley <joe@poissonconsulting.ca>
2019-09-10 14:26:37 -0700

    vld_is

--- R/chk-is.R
+++ R/chk-is.R
@@ -2,45 +2,72 @@
 #' 
 #' Checks if is a particular type of object.
 #'
-#' @details  
-#' 
-#' \code{chk_is()}: Check Is
-#' 
-#' Checks if inherits from class using:
-#' 
-#' \code{\link{inherits}(x, class)}
-#' 
-#' Class should be a string.
-#'  
 #' @inheritParams chk_true
-#' @param err Deprecated.
 #' @param class A string specifying the class.
-#' @return TRUE if passes check. Otherwise if throws an informative error unless
-#' \code{err = FALSE} in which case it returns FALSE.
+#' @return The \code{chk_} functions throw an informative error if the test fails.
+#' The \code{vld_} functions return a flag indicating whether the test was met.
+#' @name chk_is
+NULL
+
+#' @describeIn chk_is Check Is
+#'  
+#' Checks if inherits from class using \code{vld_is()}.
 #' 
 #' @export
 #' 
 #' @examples 
 #' 
 #' # chk_is
-#' try(chk_is(1, "character"))
-#' try(chk_is(NULL, "numeric"))
-#' chk_is(numeric(0), "numeric")
 #' chk_is(1, "numeric")
-#' chk_is(NA_real_, "numeric")
-#'  
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_is <- function(x, class, err = TRUE, x_name = NULL) {
-  if(inherits(x, class)) return(TRUE)
-  if(!err) return(FALSE)
+#' try(chk_is(1, "character"))
+chk_is <- function(x, class, x_name = NULL) {
+  if(vld_is(x, class)) return(invisible())
   if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
   stop(x_name, " must inherit from class '", class, "'.", call. = FALSE)
 }
 
+#' @describeIn chk_is Validate Is
+#'  
+#' Validates inherits from class using
+#' 
+#' \code{\link{inherits}(x, class)}
+#' 
+#' Class should be a string.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # vld_is
+#' vld_is(numeric(0), "numeric")
+#' vld_is(1, "numeric")
+#' vld_is(NA_real_, "numeric")
+#' vld_is(1, "character")
+#' vld_is(NULL, "numeric")
+vld_is <- function(x, class) inherits(x, class)
+
 #' @describeIn chk_is Check Whole Numeric
 #'  
-#' Checks if integer vector or double equivalent using the equivalent of:
+#' Checks if integer vector or double equivalent using \code{vld_whole_numeric()}.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # chk_whole_numeric
+#' chk_whole_numeric(1)
+#' try(chk_whole_numeric(1.1))
+chk_whole_numeric <- function(x, x_name = NULL){
+  if(vld_whole_numeric(x)) return(invisible())
+  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
+  stop(x_name, 
+      " must be a whole numeric vector (integer vector or double equivalent).",
+      call. = FALSE)
+}
+
+#' @describeIn chk_is Validate Whole Numeric
+#'  
+#' Validates integer vector or double equivalent using
 #' 
 #' \code{is.integer(x) || (is.double(x) && isTRUE(all.equal(x, as.integer(x))))}
 #' 
@@ -48,29 +75,37 @@ chk_is <- function(x, class, err = TRUE, x_name = NULL) {
 #' 
 #' @examples 
 #' 
-#' # chk_whole_numeric
-#' try(chk_whole_numeric(TRUE))
-#' try(chk_whole_numeric(1.5))
-#' chk_whole_numeric(1)
-#' chk_whole_numeric(NA_real_)
-#' chk_whole_numeric(1:2)
-#' chk_whole_numeric(double(0))
-#'    
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_whole_numeric <- function(x, err = TRUE, x_name = NULL){
-  if(is.integer(x) || (is.double(x) && isTRUE(all.equal(x, as.integer(x)))))
-    return(TRUE)
-  if(!err) return(FALSE)
-  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
-  stop(x_name, 
-      " must be a whole numeric vector (integer vector or double equivalent).",
-      call. = FALSE)
+#' # vld_whole_numeric
+#' vld_whole_numeric(1)
+#' vld_whole_numeric(NA_real_)
+#' vld_whole_numeric(1:2)
+#' vld_whole_numeric(double(0))
+#' vld_whole_numeric(TRUE)
+#' vld_whole_numeric(1.5)
+vld_whole_numeric <- function(x) {
+  is.integer(x) || (is.double(x) && isTRUE(all.equal(x, as.integer(x))))
 }
 
 #' @describeIn chk_is Check List
 #' 
-#' Checks if is a list using:
+#' Checks if is a list using \code{vld_list()}.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # chk_list
+#' chk_list(list())
+#' try(chk_list(1))
+chk_list <- function(x, x_name = NULL){
+  if(vld_list(x)) return(invisible())
+  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
+  stop(x_name, " must be a list.", call. = FALSE)
+}
+
+#' @describeIn chk_is Validate List
+#' 
+#' Validates is a list using:
 #' 
 #' \code{\link{is.list}(x)}
 #' 
@@ -78,25 +113,34 @@ chk_whole_numeric <- function(x, err = TRUE, x_name = NULL){
 #' 
 #' @examples 
 #' 
-#' # chk_list
-#' try(chk_list(1))
-#' try(chk_list(NULL))
-#' chk_list(list())
-#' chk_list(list(x = 1))
-#' chk_list(mtcars)
-#'  
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_list <- function(x, err = TRUE, x_name = NULL){
-  if(is.list(x)) return(TRUE)
-  if(!err) return(FALSE)
-  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
-  stop(x_name, " must be a list.", call. = FALSE)
-}
+#' # vld_list
+#' vld_list(list())
+#' vld_list(list(x = 1))
+#' vld_list(mtcars)
+#' vld_list(1)
+#' vld_list(NULL)
+vld_list <- function(x) is.list(x)
 
 #' @describeIn chk_is Check Function
 #' 
-#' Checks if is a function using:
+#' Checks if is a function using \code{vld_function()}.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # chk_function
+#' chk_function(mean)
+#' try(chk_function(1))
+chk_function <- function(x, x_name = NULL){
+  if(vld_function(x)) return(invisible())
+  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
+  stop(x_name, " must be a function.", call. = FALSE)
+}
+
+#' @describeIn chk_is Validate Function
+#' 
+#' Validates is a function using:
 #' 
 #' \code{\link{is.function}(x)}
 #' 
@@ -104,17 +148,9 @@ chk_list <- function(x, err = TRUE, x_name = NULL){
 #' 
 #' @examples 
 #' 
-#' # chk_function
-#' try(chk_function(1))
-#' try(chk_function(list(1)))
-#' chk_function(mean)
-#' chk_function(function(x) x)
-#'  
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_function <- function(x, err = TRUE, x_name = NULL){
-  if(is.function(x)) return(TRUE)
-  if(!err) return(FALSE)
-  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
-  stop(x_name, " must be a function.", call. = FALSE)
-}
+#' # vld_function
+#' vld_function(mean)
+#' vld_function(function(x) x)
+#' vld_function(1)
+#' vld_function(list(1))
+vld_function <- function(x) is.function(x)
--- tests/testthat/test-chk-is.R
+++ tests/testthat/test-chk-is.R
@@ -1,55 +1,78 @@
 context("chk-is")
 
+test_that("vld_is", {
+  expect_true(vld_is(1, "numeric"))
+  expect_false(vld_is(1L, "numeric"))
+  expect_true(vld_is(1L, "integer"))
+
+  x <- list()
+  class(x) <- c("a", "b")
+  expect_true(vld_is(x, "a"))
+  expect_true(vld_is(x, "b"))
+})
+
 test_that("chk_is", {
-  expect_true(chk_is(1, "numeric"))
-  expect_false(chk_is(1L, "numeric", err = FALSE))
-  expect_true(chk_is(1L, "integer"))
+  expect_null(chk_is(1L, "integer"))
+  expect_invisible(chk_is(1L, "integer"))
   expect_error(chk_is(1, "integer"), "^`1` must inherit from class 'integer'[.]$")
   expect_error(chk_is(matrix(1), "numeric"), 
                "`matrix[(]1[)]` must inherit from class 'numeric'[.]$")
   x <- list()
   class(x) <- c("a", "b")
-  expect_true(chk_is(x, "a"))
-  expect_true(chk_is(x, "b"))
   expect_error(chk_is(x, "c"), "`x` must inherit from class 'c'")
   expect_error(chk_is(x, "c", x_name = "c"), "c must inherit from class 'c'")
 })
 
-test_that("chk_whole_numeric", {
-  expect_true(chk_whole_numeric(numeric(0)))
-  expect_true(chk_whole_numeric(integer(0)))
-  expect_false(chk_whole_numeric(logical(0), err = FALSE))
-  expect_true(chk_whole_numeric(NA_integer_))
-  expect_true(chk_whole_numeric(NA_real_))
-  expect_false(chk_whole_numeric(NA, err = FALSE))
-  expect_true(chk_whole_numeric(1))
-  expect_true(chk_whole_numeric(1L))
-  expect_false(chk_whole_numeric(1.1, err = FALSE))
-  expect_false(chk_whole_numeric(-1.1, err = FALSE))
-  expect_warning(chk_whole_numeric(Inf, err = FALSE), 
+test_that("vld_whole_numeric", {
+  expect_true(vld_whole_numeric(numeric(0)))
+  expect_true(vld_whole_numeric(integer(0)))
+  expect_false(vld_whole_numeric(logical(0)))
+  expect_warning(vld_whole_numeric(Inf), 
                  "^NAs introduced by coercion to integer range$")
-  expect_true(chk_whole_numeric(c(1, 2)))
-  expect_false(chk_whole_numeric(TRUE, err = FALSE))
+  expect_true(vld_whole_numeric(NA_integer_))
+  expect_true(vld_whole_numeric(NA_real_))
+  expect_false(vld_whole_numeric(NA))
+  expect_true(vld_whole_numeric(1))
+  expect_true(vld_whole_numeric(1L))
+  expect_false(vld_whole_numeric(1.1))
+  expect_false(vld_whole_numeric(-1.1))
+  expect_true(vld_whole_numeric(c(1, 2)))
+  expect_false(vld_whole_numeric(TRUE))
+})
+
+test_that("chk_whole_numeric", {
+  expect_null(chk_whole_numeric(1L))
+  expect_invisible(chk_whole_numeric(1L))
   expect_error(chk_whole_numeric(TRUE), "^`TRUE` must be a whole numeric vector [(]integer vector or double equivalent[)][.]$")
   expect_error(chk_whole_numeric(TRUE, x_name = "c(1,2)"), "^c[(]1,2[)] must be a whole numeric vector [(]integer vector or double equivalent[)][.]$")
 })
 
-test_that("chk_list", {
-  expect_true(chk_list(list()))
-  expect_true(chk_list(list(1)))
-  expect_true(chk_list(list(x = 1)))
+test_that("vld_list", {
+  expect_true(vld_list(list()))
+  expect_true(vld_list(list(1)))
+  expect_true(vld_list(list(x = 1)))
   x <- list(x = 1)
   class(x) <- "nlist"
-  expect_true(chk_list(x))
-  expect_false(chk_list(1, err = FALSE))
+  expect_true(vld_list(x))
+  expect_false(vld_list(1))
+})
+
+test_that("chk_list", {
+  expect_null(chk_list(list()))
+  expect_invisible(chk_list(list()))
   expect_error(chk_list(1), "^`1` must be a list[.]$")
   expect_error(chk_list(1, x_name = "list()"), "^list[(][)] must be a list[.]$")
 })
 
+test_that("vld_function", {
+  expect_true(vld_function(c))
+  expect_true(vld_function(function(){}))
+  expect_false(vld_function(1))
+})
+
 test_that("chk_function", {
-  expect_true(chk_function(p))
-  expect_true(chk_function(function(){}))
-  expect_false(chk_function(1, err = FALSE))
+  expect_null(chk_function(c))
+  expect_invisible(chk_function(c))
   expect_error(chk_function(1), "^`1` must be a function[.]$")
   expect_error(chk_function(1, x_name = "function()"), "^function[(][)] must be a function[.]$")
 })
