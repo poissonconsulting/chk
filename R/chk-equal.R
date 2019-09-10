ca3d3da58bbb8a405887adc557f703b92d7adf9d
joethorley <joe@poissonconsulting.ca>
2019-09-10 15:22:11 -0700

    vld_equal

--- R/chk-equal.R
+++ R/chk-equal.R
@@ -1,73 +1,117 @@
-#' Check Equal, Identical or Equivalent
+#' Check/Validate Equal, Identical or Equivalent
 #' 
-#' Checks if two objects are equal, identical or equivalent.
+#' Checks/validates if two objects are equal, identical or equivalent.
 #'
-#' @details  
-#' 
-#' \code{chk_equal()}: Check Equal
-#' 
-#' Checks if is equal (identical within tolerance) to y using:
-#'
-#' \code{isTRUE(\link{all.equal}(x, y, tolerance))}
-#'  
 #' @inheritParams chk_true
-#' @param err Deprecated.
 #' @param y An object to check against.
 #' @param tolerance A non-negative numeric scalar.
-#' @return TRUE if passes check. Otherwise if throws an informative error unless
-#' \code{err = FALSE} in which case it returns FALSE.
+#' @return The \code{chk_} functions throw an informative error if the test fails.
+#' The \code{vld_} functions return a flag indicating whether the test was met.
+#' @name chk_equal
+NULL
+
+#' @describeIn chk_equal Check Equal
+#' 
+#' Checks if is equal (identical within tolerance) to y using \code{vld_equal()}.
 #' 
 #' @export
 #' 
 #' @examples 
 #' 
 #' # chk_equal
-#' try(chk_equal(1, 1.0000001))
 #' chk_equal(1, 1.00000001)
+#' try(chk_equal(1, 1.0000001))
 #' chk_equal(1, 1L)
-#' try(chk_equal(c(x = 1), c(y = 1L)))
 #' chk_equal(c(x = 1), c(x = 1L))
-#'  
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_equal <- function (x, y, tolerance = sqrt(.Machine$double.eps), err = TRUE,
+#' try(chk_equal(c(x = 1), c(y = 1L)))
+chk_equal <- function (x, y, tolerance = sqrt(.Machine$double.eps),
                        x_name = NULL) {
-  if(isTRUE(all.equal(x, y, tolerance))) return(TRUE)
-  if(!err) return(FALSE)
+  if(vld_equal(x, y, tolerance = tolerance)) return(invisible())
   if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
   y <- utils::capture.output(dput(y, control = "all"))
   stop(x_name, " must be equal to: ", y, ".", call. = FALSE)
 }
 
+#' @describeIn chk_equal Validate Equal
+#' 
+#' Validates is equal (identical within tolerance) to y using
+#'
+#' \code{isTRUE(\link{all.equal}(x, y, tolerance))}
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # vld_equal
+#' vld_equal(1, 1.00000001)
+#' vld_equal(1, 1.0000001)
+#' vld_equal(1, 1L)
+#' vld_equal(c(x = 1), c(x = 1L))
+#' vld_equal(c(x = 1), c(y = 1L))
+vld_equal <- function (x, y, tolerance = sqrt(.Machine$double.eps)) {
+  isTRUE(all.equal(x, y, tolerance))
+}
+
 #' @describeIn chk_equal Check Identical
 #'
-#' Checks if is identical to y using:
-#' 
-#' \code{\link{identical}(x, y)}
+#' Checks if is identical to y using \code{vld_identical()}.
 #' 
 #' @export
 #' 
 #' @examples 
 #' 
 #' # chk_identical
-#' try(chk_identical(1, 1L))
 #' chk_identical(1, 1)
-#' try(chk_identical(1, c(1,1)))
+#' try(chk_identical(1, 1L))
 #' chk_identical(c(1,1), c(1,1))
-#'  
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_identical <- function (x, y, err = TRUE, x_name = NULL) {
-  if(identical(x, y)) return(TRUE)
-  if(!err) return(FALSE)
+#' try(chk_identical(1, c(1,1)))
+chk_identical <- function (x, y, x_name = NULL) {
+  if(identical(x, y)) return(invisible())
   if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
   y <- utils::capture.output(dput(y, control = "all"))
   stop(x_name, " must be identical to: ", y, ".", call. = FALSE)
 }
 
+#' @describeIn chk_equal Validate Identical
+#'
+#' Validates is identical to y using
+#' 
+#' \code{\link{identical}(x, y)}.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # vld_identical
+#' vld_identical(1, 1L)
+#' vld_identical(1, 1)
+#' vld_identical(1, c(1,1))
+#' vld_identical(c(1,1), c(1,1))
+vld_identical <- function (x, y) identical(x, y)
+
 #' @describeIn chk_equal Check Equivalent
 #' 
-#' Checks if is equivalent (equal ignoring attributes) to y using:
+#' Checks if is equivalent (equal ignoring attributes) to y using 
+#' \code{vld_equivalent()}.
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' # chk_equivalent
+#' chk_equivalent(1, 1.00000001)
+#' try(chk_equivalent(1, 1.0000001))
+chk_equivalent <- function (x, y, tolerance = sqrt(.Machine$double.eps), 
+                            x_name = NULL) {
+  if(vld_equivalent(x, y, tolerance = tolerance)) return(invisible())
+  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
+  y <- utils::capture.output(dput(y, control = "all"))
+  stop(x_name, " must be equivalent to: ", y, ".", call. = FALSE)
+}
+
+#' @describeIn chk_equal Validate Equivalent
+#' 
+#' Validates is equivalent (equal ignoring attributes) to y using
 #'
 #' \code{\link{isTRUE}(\link{all.equal}(x, y, tolerance, check.attributes = FALSE))}
 #' 
@@ -75,20 +119,12 @@ chk_identical <- function (x, y, err = TRUE, x_name = NULL) {
 #' 
 #' @examples 
 #' 
-#' # chk_equivalent
-#' try(chk_equivalent(1, 1.0000001))
-#' chk_equivalent(1, 1.00000001)
-#' chk_equivalent(1, 1L)
-#' chk_equivalent(c(x = 1), c(y = 1L))
-#' chk_equivalent(c(x = 1), c(x = 1L))
-#'  
-#  Licence: CC0
-#  Repository: https://github.com/poissonconsulting/chk
-chk_equivalent <- function (x, y, tolerance = sqrt(.Machine$double.eps), 
-                            err = TRUE, x_name = NULL) {
-  if(isTRUE(all.equal(x, y, tolerance, check.attributes = FALSE))) return(TRUE)
-  if(!err) return(FALSE)
-  if(is.null(x_name))  x_name <- paste0("`", deparse(substitute(x)), "`")
-  y <- utils::capture.output(dput(y, control = "all"))
-  stop(x_name, " must be equivalent to: ", y, ".", call. = FALSE)
+#' # vld_equivalent
+#' vld_equivalent(1, 1.00000001)
+#' vld_equivalent(1, 1L)
+#' vld_equivalent(c(x = 1), c(y = 1L))
+#' vld_equivalent(c(x = 1), c(x = 1L))
+#' vld_equivalent(1, 1.0000001)
+vld_equivalent <- function (x, y, tolerance = sqrt(.Machine$double.eps)) {
+  isTRUE(all.equal(x, y, tolerance, check.attributes = FALSE))
 }
