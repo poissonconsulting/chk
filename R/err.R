5759bb92a37e4e875140ff7553b2798acb303cc7
joethorley <joe@poissonconsulting.ca>
2019-09-13 07:09:21 -0700

    export edit_string to message_chk

--- R/err.R
+++ R/err.R
@@ -1,21 +1,16 @@
-edit_string <- function(..., n) {
-  string <- p0(..., collapse = "")
-  if (!is.null(n)) {
-    string <- gsub("%s", if (n == 1) "" else "s", string, fixed = TRUE)
-    string <- gsub("%r", if (n == 1) "is" else "are", string, fixed = TRUE)
-    string <- gsub("%n", n, string, fixed = TRUE)
-  }
-  string
-}
-
-#' Stop, Warning and Message
-#'
-#' Wrappers on
-#' \code{\link[rlang]{abort}()}, \code{\link[rlang]{warn}()} and
-#'  \code{\link[rlang]{inform}()}.
-#'
+#' Tidyverse Style Message
+#' 
+#' Constructs a tidyverse style message by
+#' 
+#' \itemize{
+#'   \item Capitalizing the first character if possible.
+#'   \item Adding a trailing . if missing.
+#'   \item Replacing the recognized \code{sprintf}-like types.
+#' }
+#' 
 #' @section \code{sprintf}-like types:
-#' The following \code{sprintf}-like types can be used in the custom messages:
+#' 
+#' The following recognized \code{sprintf}-like types can be used in a message:
 #'
 #' \describe{
 #'   \item{\code{n}}{The value of n.}
@@ -23,9 +18,44 @@ edit_string <- function(..., n) {
 #'   \item{\code{r}}{'is' if n == 1 otherwise 'are'}
 #' }
 #'
+#' @param ... Multiple objects that are converted to a string using 
+#' \code{paste0(..., collapse = '')}.
+#' @param n The value of n for converting \code{sprintf}-like types.
+#'
+#' @return A string of the tidyverse style message.
+#' @export
+#'
+#' @examples
+#' message_chk("there %r %n", " problem%s")
+#' message_chk("there %r %n", " problem%s", n = 1)
+#' message_chk("There %r %n", " problem%s.", n = 3)
+message_chk <- function(..., n = NULL) {
+  string <- p0(..., collapse = "")
+  if(!vld_string(string)) {
+    abort("`...` must p0(..., collapse = '') to a string.",
+                                .subclass = "chk_error")
+  }
+  if (!is.null(n)) {
+    string <- gsub("%s", if (n == 1) "" else "s", string, fixed = TRUE)
+    string <- gsub("%r", if (n == 1) "is" else "are", string, fixed = TRUE)
+    string <- gsub("%n", n, string, fixed = TRUE)
+  }
+  
+  string
+}
+
+#' Stop, Warning and Message Messages
+#'
+#' The functions call \code{\link{message_chk}()} to process
+#' the message and then 
+#' \code{\link[rlang]{abort}()}, \code{\link[rlang]{warn}()} and
+#'  \code{\link[rlang]{inform}()}, respectively.
+#'  
+#'  The user can set the subclass.
+#'
 #' @inheritParams base::stop
 #' @inheritParams rlang::abort
-#' @param n The value of n for converting \code{sprintf}-like types.
+#' @inheritParams message_chk
 #' @name err
 NULL
 
@@ -36,9 +66,9 @@ NULL
 #' @examples
 #'
 #' # err
-#' try(err("this is an error"))
+#' try(err("there %r %n problem value%s", n = 2))
 err <- function(..., n = NULL, .subclass = NULL) {
-  abort(edit_string(..., n = n), .subclass = .subclass)
+  abort(message_chk(..., n = n), .subclass = .subclass)
 }
 
 #' @describeIn err Warning
@@ -48,12 +78,9 @@ err <- function(..., n = NULL, .subclass = NULL) {
 #' @examples
 #'
 #' # wrn
-#' wrn("this is a warning")
-#' wrn("there %r %n problem value%s")
-#' wrn("there %r %n problem value%s", n = 1)
 #' wrn("there %r %n problem value%s", n = 2)
 wrn <- function(..., n = NULL, .subclass = NULL) {
-  warn(edit_string(..., n = n), .subclass = .subclass)
+  warn(message_chk(..., n = n), .subclass = .subclass)
 }
 
 #' @describeIn err Message
@@ -63,10 +90,7 @@ wrn <- function(..., n = NULL, .subclass = NULL) {
 #' @examples
 #'
 #' # msg
-#' msg("this is a message")
-#' msg("there %r %n problem value%s")
-#' msg("there %r %n problem value%s", n = 1)
 #' msg("there %r %n problem value%s", n = 2)
 msg <- function(..., n = NULL, .subclass = NULL) {
-  inform(edit_string(..., n = n), .subclass = .subclass)
+  inform(message_chk(..., n = n), .subclass = .subclass)
 }
--- R/utils.R
+++ R/utils.R
@@ -1,25 +1,25 @@
 #' Abort Check
 #'
-#' A wrapper on \code{\link[rlang]{abort}(paste0(...))} that throws an error.
+#' A wrapper on \code{\link{err}} that sets the subclass to be 'chk_error'.
 #'
-#' It is exported to allow users to construct their own `chk_` functions.
+#' It is exported to allow users to easily construct their own `chk_` functions.
 #'
-#' @param ... A substituted object to deparse.
-#' @return Throws an error of class \code{chk_error}.
-#' @seealso \code{\link[rlang]{abort}}
+#' @inheritParams message_chk
+#' @return Throws an error of class \code{'chk_error'}.
+#' @seealso \code{{err}}
 #' @export
 #'
 #' @examples
 #' try(abort_chk())
 #' try(abort_chk("x must be NULL."))
-abort_chk <- function(...) abort(p0(...), .subclass = "chk_error")
+abort_chk <- function(..., n = NULL) err(..., n = n, .subclass = "chk_error")
 
 #' Deparse Tick
 #'
 #' \code{deparse_backtick} is a wrapper on \code{\link{deparse}} that
 #' also back ticks the return string.
 #'
-#' It is exported to allow users to construct their own `chk_` functions.
+#' It is exported to allow users to easily construct their own `chk_` functions.
 #'
 #' @param x A substituted object to deparse.
 #'
