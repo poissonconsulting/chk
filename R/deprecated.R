800b70901d99353552bb22a127e5d0419b88542b
joethorley <joe@poissonconsulting.ca>
2019-09-13 08:29:58 -0700

    style

--- R/deprecated.R
+++ R/deprecated.R
@@ -7,7 +7,7 @@
 NULL
 
 #' @describeIn chk_file Check Directories Exist
-#' 
+#'
 #' \lifecycle{soft-deprecated}
 #'
 #' Checks if directories exist using:
@@ -21,7 +21,7 @@ chk_dirs <- function(x) {
 }
 
 #' @describeIn chk_file Check Files Exist
-#' 
+#'
 #' \lifecycle{soft-deprecated}
 #'
 #' Checks if files exist using:
@@ -37,7 +37,7 @@ chk_files <- function(x) {
 #' @describeIn chk_unique Check Length
 #'
 #' \lifecycle{deprecated}
-#' 
+#'
 #' Checks if is length length using:
 #'
 #' \code{length(x) == length}
@@ -60,7 +60,7 @@ chk_length <- function(x, length = 1L, x_name = NULL) {
 #' @describeIn chk_deprecated Check Count
 #'
 #' \lifecycle{deprecated}
-#' 
+#'
 #' Checks if non-missing non-negative integer scalar or double equivalent using:
 #'
 #' \code{is.numeric(x) && length(x) == 1L && !anyNA(x) && x >= 0 &&
@@ -84,7 +84,7 @@ chk_count <- function(x, x_name = NULL) {
 #' @describeIn chk_deprecated Check Proportion
 #'
 #' \lifecycle{deprecated}
-#' 
+#'
 #' Checks if non-missing numeric scalar between 0 and 1 using:
 #'
 #' \code{is.numeric(x) && length(x) == 1L && !anyNA(x) && x >= 0 && x <= 1}
@@ -103,29 +103,33 @@ chk_proportion <- function(x, x_name = NULL) {
 }
 
 #' @describeIn chk_deprecated Check In
-#' 
+#'
 #' \lifecycle{soft-deprecated}
 #'
 #' Checks if all values in values using \code{chk_subset()}.
 #'
 #' @export
 chk_in <- function(x, values, x_name = NULL) {
-  deprecate_soft("0.0.1.9010", what = "chk_in()", 
-                 with = "chk_subset()")
+  deprecate_soft("0.0.1.9010",
+    what = "chk_in()",
+    with = "chk_subset()"
+  )
   if (is.null(x_name)) x_name <- deparse_backtick(substitute(x))
   chk_subset(x, values, x_name = x_name)
 }
 
 #' @describeIn chk_deprecated Check Has
-#' 
+#'
 #' \lifecycle{soft-deprecated}
 #'
 #' Checks if includes all values using \code{chk_superset()}.
 #'
 #' @export
 chk_has <- function(x, values, x_name = NULL) {
-  deprecate_soft("0.0.1.9010", what = "chk_has()", 
-                 with = "chk_superset()")
+  deprecate_soft("0.0.1.9010",
+    what = "chk_has()",
+    with = "chk_superset()"
+  )
   if (is.null(x_name)) x_name <- deparse_backtick(substitute(x))
   chk_superset(x, values, x_name = x_name)
 }
@@ -134,7 +138,7 @@ chk_has <- function(x, values, x_name = NULL) {
 #' @describeIn chk_deprecated Check Is
 #'
 #' \lifecycle{soft-deprecated}
-#' 
+#'
 #' Checks if inherits from class using \code{vld_is()}.
 #'
 #' @export
@@ -145,8 +149,10 @@ chk_has <- function(x, values, x_name = NULL) {
 #' chk_is(1, "numeric")
 #' try(chk_is(1, "character"))
 chk_is <- function(x, class, x_name = NULL) {
-  deprecate_soft("0.0.1.9018", what = "chk_is()", with = "chk_class_s3()",
-                 id = "chk_is")
+  deprecate_soft("0.0.1.9018",
+    what = "chk_is()", with = "chk_class_s3()",
+    id = "chk_is"
+  )
   if (vld_is(x, class)) {
     return(invisible())
   }
@@ -157,7 +163,7 @@ chk_is <- function(x, class, x_name = NULL) {
 #' @describeIn chk_deprecated Validate Is
 #'
 #' \lifecycle{soft-deprecated}
-#' 
+#'
 #' Validates inherits from class using
 #'
 #' \code{\link{inherits}(x, class)}
@@ -175,7 +181,9 @@ chk_is <- function(x, class, x_name = NULL) {
 #' vld_is(1, "character")
 #' vld_is(NULL, "numeric")
 vld_is <- function(x, class) {
-  deprecate_soft("0.0.1.9018", what = "vld_is()", with = "vld_class_s3()", 
-                 id = "chk_is")
+  deprecate_soft("0.0.1.9018",
+    what = "vld_is()", with = "vld_class_s3()",
+    id = "chk_is"
+  )
   inherits(x, class)
 }
--- R/err.R
+++ R/err.R
@@ -1,15 +1,15 @@
 #' Tidyverse Style Message
-#' 
+#'
 #' Constructs a tidyverse style message by
-#' 
+#'
 #' \itemize{
 #'   \item Capitalizing the first character if possible.
 #'   \item Adding a trailing . if missing.
 #'   \item Replacing the recognized \code{sprintf}-like types.
 #' }
-#' 
+#'
 #' @section \code{sprintf}-like types:
-#' 
+#'
 #' The following recognized \code{sprintf}-like types can be used in a message:
 #'
 #' \describe{
@@ -18,7 +18,7 @@
 #'   \item{\code{r}}{'is' if n == 1 otherwise 'are'}
 #' }
 #'
-#' @param ... Multiple objects that are converted to a string using 
+#' @param ... Multiple objects that are converted to a string using
 #' \code{paste0(..., collapse = '')}.
 #' @param n The value of n for converting \code{sprintf}-like types.
 #'
@@ -36,7 +36,7 @@ message_chk <- function(..., n = NULL) {
     string <- gsub("%r", if (n == 1) "is" else "are", string, fixed = TRUE)
     string <- gsub("%n", n, string, fixed = TRUE)
   }
-  if(!grepl("[.]$", string)) string <- p0(string, ".")
+  if (!grepl("[.]$", string)) string <- p0(string, ".")
   string <- p0(toupper(substr(string, 1, 1)), substr(string, 2, nchar(string)))
   string
 }
@@ -44,10 +44,10 @@ message_chk <- function(..., n = NULL) {
 #' Stop, Warning and Message Messages
 #'
 #' The functions call \code{\link{message_chk}()} to process
-#' the message and then 
+#' the message and then
 #' \code{\link[rlang]{abort}()}, \code{\link[rlang]{warn}()} and
 #'  \code{\link[rlang]{inform}()}, respectively.
-#'  
+#'
 #'  The user can set the subclass.
 #'
 #' @inheritParams base::stop
