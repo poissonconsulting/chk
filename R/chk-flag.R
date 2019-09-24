0e98008934ba2270fd68f1c8774117858907010b
joethorley <joe@poissonconsulting.ca>
2019-09-24 09:45:42 -0700

    chk-flag and chk-lgl own files

---
+++ R/chk-flag.R
@@ -0,0 +1,41 @@
+#' Check Flag
+#'
+#' @description
+#' Checks if non-missing logical scalar using 
+#' 
+#' \code{is.logical(x) && length(x) == 1L && !anyNA(x)}.
+#'
+#' @param x The object to check.
+#' @param x_name A string of the name of object x or NULL.
+#' @return The \code{chk_} function throws an informative error if the test fails.
+#' 
+#' The \code{vld_} function returns a flag indicating whether the test was met.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' # chk_flag
+#' chk_flag(TRUE)
+#' try(vld_flag(1))
+chk_flag <- function(x, x_name = NULL) {
+  if (vld_flag(x)) {
+    return(invisible())
+  }
+  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
+  abort_chk(x_name, " must be a flag (TRUE or FALSE)")
+}
+
+#' @describeIn chk_flag Validate Flag
+#'
+#' @export
+#' 
+#' @examples
+#' 
+#' # vld_flag
+#' vld_flag(TRUE)
+#' vld_flag(FALSE)
+#' vld_flag(NA)
+#' vld_flag(1)
+#' vld_flag(c(TRUE, TRUE))
+vld_flag <- function(x) is.logical(x) && length(x) == 1L && !anyNA(x)
---
+++ R/chk-lgl.R
@@ -0,0 +1,42 @@
+#' Check Logical Scalar
+#'
+#' @description
+#' Checks if logical scalar using
+#' 
+#' \code{is.logical(x) && length(x) == 1L}.
+#'
+#' @inheritParams chk_flag
+#' @return The \code{chk_} function throws an informative error if the test fails.
+#' 
+#' The \code{vld_} function returns a flag indicating whether the test was met.
+#'
+#' @seealso \code{\link{chk_flag}()}
+#' @export
+#'
+#' @examples
+#'
+#' # chk_lgl
+#' chk_lgl(NA)
+#' try(chk_lgl(1))
+chk_lgl <- function(x, x_name = NULL) {
+  if (vld_lgl(x)) {
+    return(invisible())
+  }
+  if (is.null(x_name)) x_name <- deparse_backtick_chk((substitute(x)))
+  abort_chk(x_name, " must be a logical scalar (TRUE, FALSE or NA)")
+}
+
+
+#' @describeIn chk_lgl Validate Logical Scalar
+#'
+#' @export
+#'
+#' @examples
+#'
+#' # vld_lgl
+#' vld_lgl(TRUE)
+#' vld_lgl(FALSE)
+#' vld_lgl(NA)
+#' vld_lgl(1)
+#' vld_lgl(c(TRUE, TRUE))
+vld_lgl <- function(x) is.logical(x) && length(x) == 1L
