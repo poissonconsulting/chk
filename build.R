1331879f543c6f78faf87fd8b58bfed61f15faa0
joethorley <joe@poissonconsulting.ca>
2019-10-01 16:26:02 -0700

    style package through build.R

--- build.R
+++ build.R
@@ -1,4 +1,7 @@
-styler::style_pkg()
+transformers <- styler::tidyverse_style(strict = FALSE)
+transformers$space$add_space_after_for_if_while <- NULL
+styler::style_pkg(transformers = transformers)
+
 devtools::test()
 devtools::document()
 # knitr::knit("README.Rmd")
