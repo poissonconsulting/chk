fd1ff3ba7d974b4441e5697fe88ae9d14eabaa32
Ayla Pearson <aylapearson23@gmail.com>
2019-10-21 13:08:41 -0700

    updated documentation and man for chk-ext and chk-file

--- R/chk-file.R
+++ R/chk-file.R
@@ -1,20 +1,17 @@
-#' Check/Validate File or Directory Exist
+#' Check File or Directory Exist
 #'
-#' Checks/validates if file or directory exists.
+#' @description
+#' Checks if file or directory exists using
 #'
-#' If checking/validating multiple files use `chk_all(x, chk_file)`.
+#' `vld_string(x) && file.exists(x) && !dir.exists(x)`
 #'
-#' @inheritParams chk_true
-#' @param ext A character vector of the permitted file extensions (without the .).
-#' @return The `chk_` functions throw an informative error if the test fails.
+#' @inheritParams chk_flag
+#' @return 
+#' The `chk_` functions throw an informative error if the test fails.
+#' 
 #' The `vld_` functions return a flag indicating whether the test was met.
-#' @name chk_file
-NULL
-
-#' @describeIn chk_file Check File
 #'
-#' Checks if files exist using `vld_file(x)`.
-#'
+#' @family chk_files
 #' @export
 #'
 #' @examples
@@ -35,10 +32,6 @@ chk_file <- function(x, x_name = NULL) {
 
 #' @describeIn chk_file Validate File
 #'
-#' Checks if files exist using `all(file.exists(x) && !dir.exists(x))`.
-#'
-#' @seealso [file.exists()]
-#' @seealso [dir.exists()]
 #' @export
 #'
 #' @examples
@@ -46,39 +39,3 @@ chk_file <- function(x, x_name = NULL) {
 #' # vld_file
 #' vld_file(tempfile())
 vld_file <- function(x) vld_string(x) && file.exists(x) && !dir.exists(x)
-
-#' @describeIn chk_file Check File Extension
-#'
-#' Checks extension exist using `vld_ext(x)`.
-#'
-#' The user may want to use [toupper()] or [tolower()]
-#' to ensure the case matches.
-#'
-#' @export
-#'
-#' @examples
-#'
-#' # chk_ext
-#' try(chk_ext("file1.pdf", "png"))
-chk_ext <- function(x, ext, x_name = NULL) {
-  if(vld_ext(x, ext)) {
-    return(invisible())
-  }
-  if(is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
-  chk_string(x, x_name = x_name)
-  abort_chk(x_name, " must have extension ", cc(ext, " or "), " (not '",
-    tools::file_ext(x), "')")
-}
-
-#' @describeIn chk_file Validate File Extension
-#'
-#' Checks extension using `vld_string(x) && vld_subset(tools::file_ext(x), ext)`.
-#'
-#' @export
-#'
-#' @examples
-#'
-#' # vld_ext
-#' vld_ext("oeu.pdf", "pdf")
-#' vld_ext(toupper("oeu.pdf"), "PDF")
-vld_ext <- function(x, ext) vld_string(x) && vld_subset(tools::file_ext(x), ext)
