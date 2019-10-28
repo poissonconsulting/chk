722e3337408fb76582631bf394c853c0c0dcf045
Ayla Pearson <aylapearson23@gmail.com>
2019-10-28 13:54:25 -0700

    updated documentation for chk_null family; chk_null, chk_not_null

---
+++ R/chk-not-null.R
@@ -0,0 +1,40 @@
+#' Check not NULL
+#' 
+#' @description 
+#' Checks if not NULL using
+#'
+#' `!is.null(x)`
+#' `is.null(x)`
+#'
+#' @inheritParams chk_flag 
+#' @return 
+#' The `chk_` function throws an informative error if the test fails.
+#'
+#' The `vld_` function returns a flag indicating whether the test was met.
+#' 
+#' @family chk_null
+#' @export
+#'
+#' @examples
+#'
+#' # chk_not_null
+#' try(chk_not_null(NULL))
+#' chk_not_null(1)
+chk_not_null <- function(x, x_name = NULL) {
+  if(vld_not_null(x)) {
+    return(invisible())
+  }
+  if(is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
+  abort_chk(x_name, " must not be NULL")
+}
+
+#' @describeIn chk_not_null Validate Not NULL
+#'
+#' @export
+#'
+#' @examples
+#'
+#' # vld_not_null
+#' vld_not_null(1)
+#' vld_not_null(NULL)
+vld_not_null <- function(x) !is.null(x)
--- R/chk-null.R
+++ R/chk-null.R
@@ -1,18 +1,17 @@
-#' Check/Validate NULL or not NULL
+#' Check NULL
 #'
-#' Checks/validates if NULL or not NULL.
-#'
-#' @inheritParams chk_true
-#' @return The `chk_` functions throw an informative error if the test fails.
-#' The `vld_` functions return a flag indicating whether the test was met.
-#' @name chk_null
-NULL
-
-#' @describeIn chk_null Validate NULL
-#'
+#' @description 
 #' Checks if NULL using
-#' `vld_null()`.
 #'
+#' `is.null(x)`
+#'
+#' @inheritParams chk_flag
+#' @return 
+#' The `chk_` functions throw an informative error if the test fails.
+#' 
+#' The `vld_` functions return a flag indicating whether the test was met.
+#' 
+#' @family chk_null
 #' @export
 #'
 #' @examples
@@ -30,12 +29,6 @@ chk_null <- function(x, x_name = NULL) {
 
 #' @describeIn chk_null Validate NULL
 #'
-#' Validates if NULL using
-#'
-#' `is.null(x)`.
-#'
-#' @seealso [is.null()]
-#'
 #' @export
 #'
 #' @examples
@@ -44,38 +37,3 @@ chk_null <- function(x, x_name = NULL) {
 #' vld_null(NULL)
 #' vld_null(1)
 vld_null <- function(x) is.null(x)
-
-#' @describeIn chk_null Check Not NULL
-#'
-#' Checks if not NULL using
-#' `vld_not_null()`.
-#'
-#' @export
-#'
-#' @examples
-#'
-#' # chk_not_null
-#' try(chk_not_null(NULL))
-#' chk_not_null(1)
-chk_not_null <- function(x, x_name = NULL) {
-  if(vld_not_null(x)) {
-    return(invisible())
-  }
-  if(is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
-  abort_chk(x_name, " must not be NULL")
-}
-
-#' @describeIn chk_null Validate Not NULL
-#'
-#' Validates if not NULL using
-#'
-#' `!is.null(x)`.
-#'
-#' @export
-#'
-#' @examples
-#'
-#' # vld_not_null
-#' vld_not_null(1)
-#' vld_not_null(NULL)
-vld_not_null <- function(x) !is.null(x)
