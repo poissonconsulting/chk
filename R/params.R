4c11d927f79c15525322c34e074211c6f39525f4
Ayla Pearson <aylapearson23@gmail.com>
2019-11-18 12:04:17 -0800

    updated documentation for chk_ranges family; chk_range, chk_lt, chk_lte, chk_gt, chk_gte

---
+++ R/params.R
@@ -0,0 +1,11 @@
+#' Parameter Descriptions for chk Functions
+#' @param x The object to check.
+#' @param x_name A string of the name of object x or NULL.
+#' @param chk A flag specifying whether to check the other parameters.
+#' @param value A non-missing scalar of a value.
+#' @param range A vector of length 2 of the lower and upper permitted values.
+#' 
+#' 
+#' @keywords internal
+#' @name params
+NULL
