d44f0ec5599b9108cb8326cb2a84e57b8aea65f6
Ayla Pearson <aylapearson23@gmail.com>
2019-11-18 15:12:13 -0800

    added chk_is family parameters

--- R/params.R
+++ R/params.R
@@ -2,10 +2,16 @@
 #' @param x The object to check.
 #' @param x_name A string of the name of object x or NULL.
 #' @param chk A flag specifying whether to check the other parameters.
+#' 
 #' @param value A non-missing scalar of a value.
 #' @param range A non-missing sorted vector of length 2 of the lower and upper permitted values.
+#' 
 #' @param regexp A string of a regular expression.
+#' 
 #' @param values A vector of the permitted values.
+#' 
+#' @param class A string specifying the class.
+#' @param formals A count of the number of formal arguments.
 #' 
 #' @keywords internal
 #' @name params
