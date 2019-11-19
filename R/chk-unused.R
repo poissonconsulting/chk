f7a4912ba358407e5080b5fdde881480a3776dcd
Ayla Pearson <aylapearson23@gmail.com>
2019-11-18 16:03:52 -0800

    updated documentation for chk_... family; chk_used, chk_unused

--- R/chk-unused.R
+++ R/chk-unused.R
@@ -1,18 +1,17 @@
-#' Check/Validate ... Unused or Used
+#' Check ... Unused
 #'
-#' Checks/validates if ... is unused or used.
+#' @description 
+#' Checks if ... is unused
 #'
-#' @param ... Additional arguments.
-#' @return The `chk_` functions throw an informative error if the test fails.
+#' `length(list(...)) == 0L`
+#' 
+#' @inheritParams params
+#' @return 
+#' The `chk_` functions throw an informative error if the test fails.
+#' 
 #' The `vld_` functions return a flag indicating whether the test was met.
-#' @name chk_unused
-NULL
-
-#' @describeIn chk_unused Check ... Unused
-#'
-#' Checks if is ... unused using
-#' `vld_unused()`.
-#'
+#' 
+#' @family chk_...
 #' @export
 #'
 #' @examples
@@ -33,14 +32,6 @@ chk_unused <- function(...) {
 
 #' @describeIn chk_unused Validate ... Unused
 #'
-#' Validates if is ... unused using
-#'
-#' `length(list(...)) == 0L`.
-#'
-#' @seealso [length()]
-#' @seealso [list()]
-#' @seealso [`...`]
-#'
 #' @export
 #'
 #' @examples
@@ -52,44 +43,3 @@ chk_unused <- function(...) {
 #' fun(1)
 #' try(fun(1, 2))
 vld_unused <- function(...) length(list(...)) == 0L
-
-#' @describeIn chk_unused Check ... Used
-#'
-#' Checks if is ... used using
-#' `vld_unused()`.
-#'
-#' @export
-#'
-#' @examples
-#'
-#' # chk_used
-#' fun <- function(x, ...) {
-#'   chk_used(...)
-#'   x
-#' }
-#' try(fun(1))
-#' fun(1, 2)
-chk_used <- function(...) {
-  if(vld_used(...)) {
-    return(invisible())
-  }
-  abort_chk("`...` must be used")
-}
-
-#' @describeIn chk_unused Validate ... Used
-#'
-#' Validates if ... used using
-#'
-#' `length(list(...)) != 0L`.
-#'
-#' @export
-#'
-#' @examples
-#'
-#' # vld_used
-#' fun <- function(x, ...) {
-#'   vld_used(...)
-#' }
-#' fun(1)
-#' fun(1, 2)
-vld_used <- function(...) length(list(...)) != 0L
