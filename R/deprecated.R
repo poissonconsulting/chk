c2b5f47089243d1f91b34268fd0075326518b1f0
Ayla Pearson <aylapearson23@gmail.com>
2020-01-21 11:42:30 -0800

    finished cleaning and organizing of deprecated to make it more consistent

--- R/deprecated.R
+++ R/deprecated.R
@@ -8,11 +8,28 @@
 #' @name chk_deprecated
 NULL
 
+#' @describeIn chk_deprecated Check Count
+#'
+#' \lifecycle{soft-deprecated}
+#'
+#' Replace by \code{\link{chk_whole_number}(x); \link{chk_gte}(x)}
+#'
+#' @export
+chk_count <- function(x, x_name = NULL) {
+  deprecate_soft("0.2.0",
+                 what = "chk_count()",
+                 details = "Please use `chk_whole_number()` and `chk_gte()` instead"
+  )
+  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
+  chk_whole_number(x)
+  chk_gte(x)
+}
+
 #' @describeIn chk_deprecated Check Directories Exist
 #'
 #' \lifecycle{soft-deprecated}
 #'
-#' Replace with \code{\link{chk_all}(x, \link{chk_dir})}.
+#' Replace with \code{\link{chk_all}(x, \link{chk_dir})}
 #'
 #' @export
 chk_dirs <- function(x) {
@@ -38,11 +55,81 @@ chk_files <- function(x) {
   chk_all(x, chk_file)
 }
 
+#' @describeIn chk_deprecated Check Has
+#'
+#' \lifecycle{soft-deprecated}
+#'
+#' Replace by [chk_superset()]
+#'
+#' @export
+chk_has <- function(x, values, x_name = NULL) {
+  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
+  chk_superset(x, values, x_name = x_name)
+  deprecate_soft("0.2.0",
+                 what = "chk_has()",
+                 with = "chk_superset()"
+  )
+  invisible()
+}
+
+#' @describeIn chk_deprecated Check In
+#'
+#' \lifecycle{soft-deprecated}
+#'
+#' Replace by [chk_subset()]
+#'
+#' @export
+chk_in <- function(x, values, x_name = NULL) {
+  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
+  chk_subset(x, values, x_name = x_name)
+  deprecate_soft("0.2.0",
+                 what = "chk_in()",
+                 with = "chk_subset()"
+  )
+  invisible()
+}
+
+#' @describeIn chk_deprecated Check Is
+#'
+#' \lifecycle{soft-deprecated}
+#'
+#' Replace by [chk_s3_class()] or [chk_s4_class()]
+#'
+#' @export
+chk_is <- function(x, class, x_name = NULL) {
+  deprecate_soft("0.2.0",
+                 what = "chk_is()",
+                 details = "Please use `chk_s3_class()` or `chk_s4_class()` instead",
+                 id = "chk_is"
+  )
+  if (vld_is(x, class)) {
+    return(invisible())
+  }
+  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
+  abort_chk(x_name, " must inherit from class '", class, "'")
+}
+
+#' @describeIn chk_deprecated Validate Is
+#'
+#' \lifecycle{soft-deprecated}
+#' 
+#' Replace by [vld_s3_class()] or [vld_s4_class()]
+#'
+#' @param class A string of the class of the object.
+#' 
+#' @export
+vld_is <- function(x, class) {
+  deprecate_soft("0.2.0",
+                 what = "vld_is()",
+                 details = "Please use `vld_s3_class()` or `vld_s4_class()` instead",
+                 id = "chk_is"
+  )
+  inherits(x, class)
+}
+
 #' @describeIn chk_deprecated Check Length
 #'
-#' \lifecycle{deprecated}
-#'
-#' `length` should be a count.
+#' \lifecycle{soft-deprecated}
 #'
 #' Replace by \code{\link{chk_range}(length(x))}
 #'
@@ -61,27 +148,88 @@ chk_length <- function(x, length = 1L, x_name = NULL) {
   abort_chk(x_name, " must be length ", length, ", not ", length(x))
 }
 
-
-#' @describeIn chk_deprecated Check Count
+#' @describeIn chk_deprecated Check No Missing Values
 #'
-#' \lifecycle{deprecated}
-#'
-#' Replace by \code{\link{chk_whole_number}(x); \link{chk_gte}(x)}
+#' \lifecycle{soft-deprecated}
+#' 
+#' Replace with [chk_not_any_na()]
 #'
 #' @export
-chk_count <- function(x, x_name = NULL) {
-  deprecate_warn("0.2.0",
-    what = "chk_count()",
-    details = "Please use `chk_whole_number()` and `chk_gte()` instead"
+chk_no_missing <- function(x, x_name = NULL) {
+  deprecate_soft("0.2.0",
+                 what = "chk_no_missing()", with = "chk_not_any_na()",
+                 id = "chk_no_missing"
   )
+  if (vld_no_missing(x)) {
+    return(invisible())
+  }
   if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
-  chk_whole_number(x)
-  chk_gte(x)
+  abort_chk(x_name, " must not have missing values")
+}
+
+#' @describeIn chk_deprecated Validate No Missing Values
+#'
+#' \lifecycle{soft-deprecated}
+#' 
+#' Replace with [vld_not_any_na()]
+#'
+#' @export
+vld_no_missing <- function(x) {
+  deprecate_soft("0.2.0",
+                 what = "vld_no_missing()", with = "vld_not_any_na()",
+                 id = "chk_no_missing"
+  )
+  !anyNA(x)
+}
+
+#' @describeIn chk_deprecated Turns checking off
+#'
+#' \lifecycle{deprecated}
+#' 
+#' This approach is no longer recommended
+#'
+#' @export
+chk_off <- function() {
+  deprecate_warn("0.2.0",
+                 what = "chk_off()",
+                 details = "This approach is no longer recommended"
+  )
+  options(chk.on = FALSE)
+}
+
+#' @describeIn chk_deprecated Turns checking on
+#'
+#' \lifecycle{deprecated}
+#' 
+#' This approach is no longer recommended
+#'
+#' @export
+chk_on <- function() {
+  deprecate_warn("0.2.0",
+                 what = "chk_on()",
+                 details = "This approach is no longer recommended"
+  )
+  options(chk.on = TRUE)
+}
+
+#' @describeIn chk_deprecated Tests checking on
+#'
+#' \lifecycle{deprecated}
+#' 
+#' This approach is no longer recommended
+#'
+#' @export
+is_chk_on <- function() {
+  deprecate_warn("0.2.0",
+                 what = "is_chk_on()",
+                 details = "This approach is no longer recommended"
+  )
+  getOption("chk.on", TRUE)
 }
 
 #' @describeIn chk_deprecated Check Proportion
 #'
-#' \lifecycle{deprecated}
+#' \lifecycle{soft-deprecated}
 #'
 #' Replace by \code{\link{chk_number}(x); \link{chk_range}(x)}
 #'
@@ -96,113 +244,11 @@ chk_proportion <- function(x, x_name = NULL) {
   chk_range(x)
 }
 
-#' @describeIn chk_deprecated Check In
+#' @describeIn chk_deprecated Deparse Backtick
 #'
 #' \lifecycle{soft-deprecated}
-#'
-#' Replace by \code{\link{chk_subset}(x, values, x_name=x_name)}
-#'
-#' @export
-chk_in <- function(x, values, x_name = NULL) {
-  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
-  chk_subset(x, values, x_name = x_name)
-  deprecate_soft("0.2.0",
-    what = "chk_in()",
-    with = "chk_subset()"
-  )
-  invisible()
-}
-
-#' @describeIn chk_deprecated Check Has
-#'
-#' \lifecycle{soft-deprecated}
-#'
-#' Replace by \code{\link{chk_superset}(x, values, x_name = x_name)}
-#'
-#' @export
-chk_has <- function(x, values, x_name = NULL) {
-  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
-  chk_superset(x, values, x_name = x_name)
-  deprecate_soft("0.2.0",
-    what = "chk_has()",
-    with = "chk_superset()"
-  )
-  invisible()
-}
-
-
-#' @describeIn chk_deprecated Check Is
-#'
-#' \lifecycle{soft-deprecated}
-#'
-#' Replace by [chk_s3_class()] or [chk_s4_class()].
-#'
-#' @export
-chk_is <- function(x, class, x_name = NULL) {
-  deprecate_soft("0.2.0",
-    what = "chk_is()",
-    details = "Please use `chk_s3_class()` or `chk_s4_class()` instead",
-    id = "chk_is"
-  )
-  if (vld_is(x, class)) {
-    return(invisible())
-  }
-  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
-  abort_chk(x_name, " must inherit from class '", class, "'")
-}
-
-#' @describeIn chk_deprecated Validate Is
-#'
-#' \lifecycle{soft-deprecated}
-#'
-#' Class should be a string.
-#'
-#' @export
-vld_is <- function(x, class) {
-  deprecate_soft("0.2.0",
-    what = "vld_is()",
-    with = "vld_class_s3()",
-    id = "chk_is"
-  )
-  inherits(x, class)
-}
-
-#' @describeIn chk_deprecated Check No Missing Values
-#'
-#' \lifecycle{soft-deprecated}
-#'
-#' Checks if no missing values using
-#' `vld_no_missing()`.
-#'
-#' @export
-chk_no_missing <- function(x, x_name = NULL) {
-  deprecate_soft("0.2.0",
-    what = "chk_no_missing()", with = "chk_not_any_na()",
-    id = "chk_no_missing"
-  )
-  if (vld_no_missing(x)) {
-    return(invisible())
-  }
-  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
-  abort_chk(x_name, " must not have missing values")
-}
-
-#' @describeIn chk_deprecated Validate No Missing Values
-#'
-#' \lifecycle{soft-deprecated}
-#'
-#' @export
-vld_no_missing <- function(x) {
-  deprecate_soft("0.2.0",
-    what = "vld_no_missing()", with = "vld_not_any_na()",
-    id = "chk_no_missing"
-  )
-  !anyNA(x)
-}
-
-#' @describeIn deparse_backtick_chk Deparse Backtick
-#'
-#' \lifecycle{soft-deprecated}
+#' 
+#' Replace with [deparse_backtick_chk()]
 #'
 #' @export
 deparse_backtick <- function(x) {
@@ -211,44 +257,4 @@ deparse_backtick <- function(x) {
     with = "deparse_backtick_chk()"
   )
   deparse_backtick_chk(x)
-}
-
-
-#' @describeIn chk_deprecated Turns checking on
-#'
-#' \lifecycle{soft-deprecated}
-#'
-#' @export
-chk_on <- function() {
-  deprecate_warn("0.2.0",
-    what = "chk_on()",
-    details = "This approach is no longer recommended"
-  )
-  options(chk.on = TRUE)
-}
-
-#' @describeIn chk_deprecated Turns checking off
-#'
-#' \lifecycle{soft-deprecated}
-#'
-#' @export
-chk_off <- function() {
-  deprecate_warn("0.2.0",
-    what = "chk_off()",
-    details = "This approach is no longer recommended"
-  )
-  options(chk.on = FALSE)
-}
-
-#' @describeIn chk_deprecated Tests checking on
-#'
-#' \lifecycle{soft-deprecated}
-#'
-#' @export
-is_chk_on <- function() {
-  deprecate_warn("0.2.0",
-    what = "is_chk_on()",
-    details = "This approach is no longer recommended"
-  )
-  getOption("chk.on", TRUE)
 }
