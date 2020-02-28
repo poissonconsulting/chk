63e517bd9cf7f5de4f5d0573b26776185feadc9c
joethorley <joe@poissonconsulting.ca>
2020-02-28 04:12:11 -0800

    - Generalize check_length() to check_dim().

--- R/check-length.R
+++ R/check-length.R
@@ -1,10 +1,12 @@
-#' Check Length
+#' Check Dimension
 #'
 #' @description
-#' Checks length of an object.
+#' Checks dimension of an object.
 #'
 #' @inheritParams params
+#' @param dim A function returning a non-negative scalar integer of the dimension.
 #' @param values A flag or a whole numeric vector of the value, value range or possible values.
+#' @param dim_name A string of the name of the dim function.
 #' @return An informative error if the test fails.
 #' @family check
 #' @export
@@ -13,22 +15,26 @@
 #' try(check_length(1, values = FALSE))
 #' try(check_length(1, values = c(10, 2)))
 #' try(check_length(1, values = c(10, 2, 10)))
-check_length <- function(x, values = TRUE, x_name = NULL) {
+check_length <- function(x, dim = length, values = TRUE, x_name = NULL, dim_name = NULL) {
+  chk_function(dim)
   chkor(chk_flag(values), chk_whole_numeric(values))
   chk_not_empty(values)
-  chkor(is.null(x_name), chk_string(x_name))
 
-  if (is.null(x_name)) x_name <- deparse_backtick_chk((substitute(x)))
-  x_name <- p0("`length(", unbacktick_chk(x_name), ")`")
+  if (is.null(x_name)) x_name <- deparse((substitute(x)))
+  chk_string(x_name)
+  if (is.null(dim_name)) dim_name <- deparse((substitute(dim)))
+  chk_string(dim_name)
 
-  if(vld_true(values)) return(chk_gt(length(x), x_name = x_name))
+  x_name <- backtick_chk(p0(dim_name, "(", x_name, ")"))
+
+  if(vld_true(values)) return(chk_gt(dim(x), x_name = x_name))
   if(vld_false(values)) values <- 0L
   if(identical(length(values), 2L)) {
     if(!identical(values[1], values[2]))
-      return(chk_range(length(x), sort(values), x_name = x_name))
+      return(chk_range(dim(x), sort(values), x_name = x_name))
     values <- unique(values)
   }
   if(identical(length(values), 1L))
-    return(chk_equal(length(x), values, x_name = x_name))
-  chk_subset(length(x), sort(unique(values)), x_name = x_name)
+    return(chk_equal(dim(x), values, x_name = x_name))
+  chk_subset(dim(x), sort(unique(values)), x_name = x_name)
 }
--- tests/testthat/test-check-length.R
+++ tests/testthat/test-check-length.R
@@ -3,20 +3,20 @@ test_that("check_length", {
   expect_invisible(check_length(1))
   expect_chk_error(check_length(integer(0)),
                    "^`length[(]integer[(]0[)][)]` must be greater than 0, not 0[.]$")
-  expect_chk_error(check_length(1, FALSE),
+  expect_chk_error(check_length(1, values = FALSE),
                    "^`length[(]1[)]` must be equal to: 0L[.]")
-  expect_null(check_length(2, 1))
-  expect_chk_error(check_length(1, 2),
+  expect_null(check_length(2, values = 1))
+  expect_chk_error(check_length(1, values = 2),
                    "^`length[(]1[)]` must be equal to: 2[.]")
-  expect_chk_error(check_length(1, 2:3),
+  expect_chk_error(check_length(1, values = 2:3),
                    "^`length[(]1[)]` must be between 2 and 3, not 1[.]")
-  expect_chk_error(check_length(1, 3:2),
+  expect_chk_error(check_length(1, values = 3:2),
                    "^`length[(]1[)]` must be between 2 and 3, not 1[.]")
-  expect_null(check_length(1:2, 2:3))
-  expect_chk_error(check_length(2, c(2,2)),
+  expect_null(check_length(1:2, values = 2:3))
+  expect_chk_error(check_length(2, values = c(2,2)),
                    "`length[(]2[)]` must be equal to: 2[.]")
-  expect_chk_error(check_length(2, c(2,2,2)),
+  expect_chk_error(check_length(2, values = c(2,2,2)),
                    "`length[(]2[)]` must match 2, not 1[.]")
-  expect_chk_error(check_length(2, c(3,2,2)),
+  expect_chk_error(check_length(2, values = c(3,2,2)),
                    "`length[(]2[)]` must match 2 or 3, not 1[.]")
 })
