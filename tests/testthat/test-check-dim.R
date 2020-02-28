295880fcd6506ba562354b3d79385625b9d8f4d1
joethorley <joe@poissonconsulting.ca>
2020-02-28 04:47:14 -0800

    more test check_dim

--- tests/testthat/test-check-dim.R
+++ tests/testthat/test-check-dim.R
@@ -1,6 +1,8 @@
-test_that("check_dim", {
+test_that("check_dim length", {
   expect_null(check_dim(1))
   expect_invisible(check_dim(1))
+  expect_chk_error(check_dim(1:2, dim = identity),
+                   "`dim[(]x[)]` must be a whole number [(]non-missing integer scalar or double equivalent[)][.]")
   expect_chk_error(check_dim(integer(0)),
                    "^`length[(]integer[(]0[)][)]` must be greater than 0, not 0[.]$")
   expect_chk_error(check_dim(1, values = FALSE),
@@ -19,4 +21,16 @@ test_that("check_dim", {
                    "`length[(]2[)]` must match 2, not 1[.]")
   expect_chk_error(check_dim(2, values = c(3,2,2)),
                    "`length[(]2[)]` must match 2 or 3, not 1[.]")
+})
+
+test_that("check_dim nrow", {
+  expect_null(check_dim(data.frame(x = 1), dim = nrow))
+  expect_invisible(check_dim(data.frame(x = 1), dim = nrow))
+  expect_chk_error(check_dim(data.frame(x = integer(0)), dim = nrow),
+                   "`nrow[(]data.frame[(]x = integer[(]0[)][)][)]` must be greater than 0, not 0[.]")
+  expect_chk_error(check_dim(data.frame(x = integer(0)), dim = nrow, dim_name = "ncol"),
+                   "`ncol[(]data.frame[(]x = integer[(]0[)][)][)]` must be greater than 0, not 0[.]")
+    expect_chk_error(check_dim(data.frame(x = integer(0)), dim = nrow, x_name = "y", dim_name = "ncol"),
+                   "`ncol[(]y[)]` must be greater than 0, not 0[.]")
+
 })
