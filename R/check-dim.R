d667ad92004e3bec0d4fcb7ea2d2d167a0e461b7
joethorley <joe@poissonconsulting.ca>
2020-03-01 04:45:14 -0800

    check dim test returns non-negative whole number

--- R/check-dim.R
+++ R/check-dim.R
@@ -4,7 +4,7 @@
 #' Checks dimension of an object.
 #'
 #' @inheritParams params
-#' @param dim A function returning a whole number of the dimension.
+#' @param dim A function returning a non-negative whole number of the dimension.
 #' @param values A flag or a whole numeric vector of the value, value range or possible values.
 #' @param dim_name A string of the name of the dim function.
 #' @return An informative error if the test fails.
@@ -14,11 +14,12 @@
 #' check_dim(1)
 #' try(check_dim(1, values = FALSE))
 #' try(check_dim(1, values = c(10, 2)))
-#' try(check_dim(1, values = c(10, 2, 10)))
+#' try(check_dim(data.frame(x = 1), dim = nrow, values = c(10, 10, 2)))
 check_dim <- function(x, dim = length, values = numeric(0), x_name = NULL, dim_name = NULL) {
   chk_function(dim)
   chkor(chk_flag(values), chk_whole_numeric(values))
   chk_whole_number(dim(x))
+  chk_gte(dim(x))
 
   if (is.null(x_name)) x_name <- deparse((substitute(x)))
   chk_string(x_name)
