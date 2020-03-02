cbbc1d466c705a5fac8c9a726b9e52f5cfb85534
joethorley <joe@poissonconsulting.ca>
2020-03-02 09:36:28 -0800

    - Added chk_double() and vld_double().

---
+++ R/chk-double.R
@@ -0,0 +1,40 @@
+#' Check Double
+#'
+#' @description
+#' Checks if double using
+#'
+#' `is.double(x)`
+#'
+#' @inheritParams chk_flag
+#' @inherit params return
+#'
+#' @family chk_is
+#'
+#' @examples
+#' # chk_double
+#' chk_double(1)
+#' try(chk_double(1L))
+#'
+#' @export
+chk_double <- function(x, x_name = NULL) {
+  if (vld_double(x)) {
+    return(invisible())
+  }
+  if (is.null(x_name)) x_name <- deparse_backtick_chk((substitute(x)))
+  abort_chk(x_name, " must be double", x = x)
+}
+
+#' @describeIn chk_double Validate Double
+#'
+#' @examples
+#' # vld_double
+#' vld_double(1)
+#' vld_double(matrix(c(1,2,3,4), nrow = 2L))
+#' vld_double(double(0))
+#' vld_double(numeric(0))
+#' vld_double(NA_real_)
+#' vld_double(1L)
+#' vld_double(TRUE)
+#'
+#' @export
+vld_double <- function(x) is.double(x)
