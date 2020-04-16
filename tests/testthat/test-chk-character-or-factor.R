aad36af93c3ab13ee53d4307104302061c021d65
joethorley <joe@poissonconsulting.ca>
2020-04-16 11:47:05 -0700

    fix test message

--- tests/testthat/test-chk-character-or-factor.R
+++ tests/testthat/test-chk-character-or-factor.R
@@ -1,4 +1,4 @@
-test_that("vld_character", {
+test_that("vld_character_or_factor", {
   expect_true(vld_character_or_factor("1"))
   expect_true(vld_character_or_factor(matrix("a")))
   expect_true(vld_character_or_factor(character(0)))
@@ -8,7 +8,7 @@ test_that("vld_character", {
   expect_true(vld_character_or_factor(factor("text")))
 })
 
-test_that("chk_character", {
+test_that("chk_character_or_factor", {
   expect_null(chk_character_or_factor("1"))
   expect_invisible(chk_character_or_factor("1"))
   expect_chk_error(
