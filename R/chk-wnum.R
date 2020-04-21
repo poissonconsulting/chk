90728ad1690682cf5e38eea2390ef42f791790b5
joethorley <joe@poissonconsulting.ca>
2020-04-21 15:09:12 -0700

    - Added chk_wnum() and vld_wnum() to check whole numeric scalars.

---
+++ R/chk-wnum.R
@@ -0,0 +1,38 @@
+#' Check Whole Numeric Scalar
+#'
+#' @description
+#' Checks if whole numeric scalar using
+#'
+#' `is.numeric(x) && length(x) == 1L && (is.integer(x) || vld_true(all.equal(x, trunc(x))))`
+#'
+#' @inheritParams params
+#' @inherit params return
+#'
+#' @family chk_wnum
+#'
+#' @examples
+#' # chk_wnum
+#' chk_wnum(1)
+#' try(chk_wnum(1.1))
+#' @export
+chk_wnum <- function(x, x_name = NULL) {
+  if (vld_wnum(x)) {
+    return(invisible())
+  }
+  if (is.null(x_name)) x_name <- deparse_backtick_chk((substitute(x)))
+  abort_chk(x_name, " must be a whole numeric scalar", x = x)
+}
+
+#' @describeIn chk_wnum Validate Double
+#'
+#' @examples
+#' # vld_wnum
+#' vld_wnum(1)
+#' vld_wnum(double(0))
+#' vld_wnum(NA_real_)
+#' vld_wnum(c(1,1))
+#' vld_wnum(1L)
+#' @export
+vld_wnum <- function(x) {
+  is.numeric(x) && length(x) == 1L && (is.integer(x) || vld_true(all.equal(x, trunc(x))))
+}
---
+++ tests/testthat/test-chk-wnum.R
@@ -0,0 +1,20 @@
+context("chk-number")
+
+test_that("vld_wnum", {
+  expect_false(vld_wnum(numeric(0)))
+  expect_true(vld_wnum(NA_integer_))
+  expect_true(vld_wnum(1))
+  expect_true(vld_wnum(1L))
+  expect_false(vld_wnum(1.1))
+  expect_true(vld_wnum(-1))
+  expect_true(vld_wnum(Inf))
+  expect_false(vld_wnum(c(1, 2)))
+  expect_false(vld_wnum(TRUE))
+})
+
+test_that("chk_wnum", {
+  expect_null(chk_wnum(1))
+  expect_invisible(chk_wnum(1))
+  expect_chk_error(chk_wnum(TRUE), "^`TRUE` must be a whole numeric scalar[.]$")
+  expect_chk_error(chk_wnum(TRUE, x_name = 1L), "^1 must be a whole numeric scalar[.]$")
+})
