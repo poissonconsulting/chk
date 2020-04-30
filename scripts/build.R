67b01c4bc72fff95d16bdaf88a52c1e415eabbde
joethorley <joe@poissonconsulting.ca>
2020-04-30 07:26:01 -0700

    update build script

--- scripts/build.R
+++ scripts/build.R
@@ -2,9 +2,10 @@ roxygen2md::roxygen2md()
 
 styler::style_pkg(filetype = c("R", "Rmd"))
 
+lintr::lint_package()
 devtools::test()
 devtools::document()
-# knitr::knit("README.Rmd")
+rmarkdown::render("README.Rmd", output_format = "md_document")
 if(FALSE) {
   if(file.exists("DESCRIPTION")) unlink("docs", recursive = TRUE)
   pkgdown::build_site()
