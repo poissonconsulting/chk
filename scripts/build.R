e35ee4fcbb946ac3869c82e2e5543c34dde41226
Joe Thorley <joe@poissonconsulting.ca>
2020-05-29 08:48:26 -0700

    fix website

--- scripts/build.R
+++ scripts/build.R
@@ -1,13 +1,11 @@
 roxygen2md::roxygen2md()
-
 styler::style_pkg(filetype = c("R", "Rmd"))
-
 lintr::lint_package()
+
 devtools::test()
 devtools::document()
+
 rmarkdown::render("README.Rmd", output_format = "md_document")
-if(FALSE) {
-  if(file.exists("DESCRIPTION")) unlink("docs", recursive = TRUE)
-  pkgdown::build_site()
-}
+pkgdown::build_site()
+
 devtools::check()
