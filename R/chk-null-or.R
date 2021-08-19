bb5ea66d2eb9ee5dd50ce9b2eb67d5005a43d275
Joe Thorley <joe@poissonconsulting.ca>
2021-08-19 06:52:36 -0700

    - Fix bug `chk_null_or()` now returns (invisible) x. - Speed up `chk_null_or()`.

--- R/chk-null-or.R
+++ R/chk-null-or.R
@@ -16,13 +16,15 @@
 #' try(chk_null_or("1", chk_number))
 chk_null_or <- function(x, chk, ..., x_name = NULL) {
   if (is.null(x)) {
-    return(invisible())
+    return(invisible(x))
   }
-  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
-
   try <- try_chk(chk(x, ..., x_name = x_name))
   if (is.null(try)) {
     return(invisible(x))
+  }
+  if (is.null(x_name)) {
+    x_name <- deparse_backtick_chk(substitute(x))
+    try <- try_chk(chk(x, ..., x_name = x_name))
   }
   msg <- try_msg(try)
   msg <- sub("[.]$", " or NULL.", msg)
