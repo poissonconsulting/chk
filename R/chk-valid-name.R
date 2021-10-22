11a3dd7beb7c93098cc1fd308497d5193399f136
Joe Thorley <joe@poissonconsulting.ca>
2021-10-22 08:42:32 -0700

    - Added `chk_valid_name()`.

---
+++ R/chk-valid-name.R
@@ -0,0 +1,32 @@
+#' Check Valid Name
+#'
+#' @description
+#' Checks if valid name using
+#'
+#' `identical(make.names(x[!is.na(x)]), as.character(x[!is.na(x)]))`
+#'
+#' @inheritParams params
+#' @inherit params return
+#'
+#' @family chk_misc
+#'
+#' @examples
+#' # chk_valid_name
+#' chk_valid_name("text")
+#' try(chk_valid_name(".1"))
+#' @export
+chk_valid_name <- function(x, x_name = NULL) {
+  if (vld_valid_name(x)) {
+    return(invisible(x))
+  }
+  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
+  abort_chk(x_name, " must be valid name%s", n = length(x))
+}
+
+#' @describeIn chk_valid_name Validate Valid Name
+#'
+#' @examples
+#' # vld_valid_name
+#' vld_valid_name(".1")
+#' @export
+vld_valid_name <- function(x) identical(make.names(x[!is.na(x)]), as.character(x[!is.na(x)]))
---
+++ tests/testthat/test-chk-valid-name.R
@@ -0,0 +1,18 @@
+test_that("vld_valid_name", {
+  expect_true(vld_valid_name(numeric(0)))
+  expect_true(vld_valid_name(NA))
+  expect_true(vld_valid_name("name"))
+  expect_true(vld_valid_name(c("name", NA)))
+  expect_true(vld_valid_name(c("name", "name")))
+  expect_true(vld_valid_name("."))
+  expect_false(vld_valid_name(".1"))
+  expect_false(vld_valid_name("1"))
+  expect_false(vld_valid_name(c("1", NA)))
+})
+
+test_that("chk_valid_name", {
+  expect_identical(chk_valid_name("x"), "x")
+  expect_invisible(chk_valid_name("x"))
+  expect_chk_error(chk_valid_name(".1"), '^`".1"` must be valid name\\.$')
+  expect_chk_error(chk_valid_name(".1", x_name = 1L), '^1 must be valid name\\.$')
+})
