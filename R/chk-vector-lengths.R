ce0a4b3afc7f58c9b89130b76adc7884bb861da6
Ayla Pearson <ayla@poissonconsulting.ca>
2023-01-04 10:20:07 -0800

    undo changes as commited on wrong branch rolled back to SHA ba195d464a86f97d69ea66656b6aa89ee608cdbf

--- R/chk-vector-lengths.R
+++ R/chk-vector-lengths.R
@@ -1,39 +1,58 @@
 #' Check Vectors have the Same Length
 #'
-#' To combine vectors they either need to all be the same length or be of length one. If they are length one then the value will be recycled in the longer vectors.
+#' To combine vectors they either need to all be the same length or be of length
+#' one. If they are length one then the value will be recycled in the longer
+#' vectors.
 #'
 #' @param ... The vectors to check the lengths of.
 #'
-#' @return
+#' @return The `chk_` function throws an informative error if the test fails.
 #' @export
 #'
 #' @examples
 #' a <- c(1, 2, 3)
 #' b <- c(1, 2, 3)
-#' chk_vector_lengths(a, b)
+#' chk_lengths_into(a, b)
 #'
 #' hour <- c(7, 6, 8)
 #' min <- c(30, 45, 30)
 #' sec <- 0
-#' chk_vector_lengths(hour, min, sec)
-chk_vector_lengths <- function(...) {
-  if (vld_vector_lengths(...)) {
-    return(invisible(NULL))
+#' chk_lengths_into(hour, min, sec)
+chk_lengths_into <- function(...) {
+  if (vld_lengths_into(...)) {
+    return(invisible())
   }
+
+  vec_lengths <-lengths(list(...))
+  length_set <- unique(vec_lengths)
+
+  abort_chk(
+    "vectors must be all the same length or length 1 but not lengths ",
+    cc(length_set, conj = " and ")
+  )
 }
 
-#' @describeIn chk_vector_lengths Check Vectors have the Same Length
+#' @describeIn chk_lengths_into Check Vectors have the Same Length
 #' @export
 #' @examples
 #' a <- c(1, 2, 3)
 #' b <- c(1, 2, 3)
-#' vld_vector_lengths(a, b)
+#' vld_lengths_into(a, b)
 #'
 #' hour <- c(7, 6, 8)
 #' min <- c(30, 45, 30)
 #' sec <- 0
-#' vld_vector_lengths
-vld_vector_lengths <- function(...) {
+#' vld_lengths_into
+vld_lengths_into <- function(...) {
+  vec_lengths <- lengths(list(...))
+  length_set <- unique(vec_lengths)
 
+  if (length(length_set) == 1) {
+    return(TRUE)
+  }
+
+  if (length(length_set) >= 3) {
+    return(FALSE)
+  }
+  any(length_set == 1)
 }
-
