92a0c7d8c8d7e99cf043e738450604a8b0cddebb
Ayla Pearson <ayla@poissonconsulting.ca>
2023-01-04 11:23:05 -0800

    updating docs

--- R/chk-lengths-into.R
+++ R/chk-lengths-into.R
@@ -1,15 +1,23 @@
-#' Check Vectors have the Same Length
+#' Check Vectors Compatible Lengths
 #'
-#' To combine vectors they either need to all be the same length or be of length
-#' one. If they are length one then the value will be recycled in the longer
-#' vectors.
+#' Check vectors have the same length or are length one.
 #'
 #' @param ... The vectors to check the lengths of.
 #'
 #' @return The `chk_` function throws an informative error if the test fails.
 #' @export
+#' @details This function checks if the length of each vector is the same or
+#'   if the length is one. This function helps to ensure you are not recycling
+#'   values in the vector. For example the function will error if you
+#'   had a vector of length 2 and length 4, even though the vector of length 2
+#'   could be recycled to match up to the vector of length 4 when combined.
+#'
+#'   The intent of the function is to ensure when vectors are combined that the
+#'   lengths are the same so the outputs do not have parts that are recycled
+#'   except in the case of a single value which is a special case.
 #'
 #' @examples
+#' # chk_
 #' a <- c(1, 2, 3)
 #' b <- c(1, 2, 3)
 #' chk_lengths_into(a, b)
@@ -18,6 +26,12 @@
 #' min <- c(30, 45, 30)
 #' sec <- 0
 #' chk_lengths_into(hour, min, sec)
+#'
+#' tag <- c(14, 16, 28, 42)
+#' color_code <- c("G", "G" , "B", "B")
+#' type <- "p"
+#' chk_lengths_into(tag, color_code, type)
+#'
 chk_lengths_into <- function(...) {
   if (vld_lengths_into(...)) {
     return(invisible())
@@ -35,6 +49,7 @@ chk_lengths_into <- function(...) {
 #' @describeIn chk_lengths_into Check Vectors have the Same Length
 #' @export
 #' @examples
+#' # vld_
 #' a <- c(1, 2, 3)
 #' b <- c(1, 2, 3)
 #' vld_lengths_into(a, b)
