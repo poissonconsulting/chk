5fe26d23791f9722b225f6bbefa41c68ebe7ff7e
Ayla Pearson <ayla@poissonconsulting.ca>
2023-01-03 16:31:27 -0800

    adding tests for chk portion of checking vector lengths

--- tests/testthat/test-chk-vector-lengths.R
+++ tests/testthat/test-chk-vector-lengths.R
@@ -56,9 +56,67 @@ test_that("validate false if list passed", {
 
 
 # chk -----
-test_that("passes when single vector or non zero length passed", {
-  x <- c(1, 3, 5)
-  chk_vector_lengths(x)
+test_that("passes when single value passed", {
+  x <- 1
+  expect_null(chk_vector_lengths(x))
+  expect_invisible(chk_vector_lengths(x))
 })
 
+test_that("passes when single vector passed", {
+  x <- c(1, 3, 5)
+  expect_null(chk_vector_lengths(x))
+  expect_invisible(chk_vector_lengths(x))
+})
 
+test_that("passes when multiple vectors of same length passed", {
+  x <- c(1, 3, 5)
+  y <- c(2, 4, 6)
+  z <- c(7, 8, 0)
+  expect_null(chk_vector_lengths(x, y, z))
+  expect_invisible(chk_vector_lengths(x, y, z))
+})
+
+test_that("passes when multiple vectors of same length passed and vectors of
+          length one passed", {
+  x <- c(1, 3, 5)
+  y <- c(2, 4, 6)
+  z <- 7
+  expect_null(chk_vector_lengths(x, y, z))
+  expect_invisible(chk_vector_lengths(x, y, z))
+})
+
+test_that("throws error when vector of length zero passed", {
+  x <- NULL
+  expect_error(
+    chk_vector_lengths(x),
+    regexp = "vectors of length zero are not allowed"
+  )
+})
+
+test_that("throws error when vector of length zero passed", {
+  x <- NULL
+  y <- c(1, 2, 3)
+  expect_error(
+    chk_vector_lengths(x, y),
+    regexp = "vectors of length zero are not allowed"
+  )
+})
+
+test_that("throws error when vector have two different lengths", {
+  x <- c(4, 5)
+  y <- c(1, 2, 3)
+  expect_error(
+    chk_vector_lengths(x, y),
+    regexp = "vectors must be all the same length not lengths 2 and 3"
+  )
+})
+
+test_that("throws error when vector have three different lengths", {
+  x <- c(4, 5)
+  y <- c(1, 2, 3)
+  z <- c(6, 7, 8, 9)
+  expect_error(
+    chk_vector_lengths(x, y, z),
+    regexp = "vectors must be all the same length not lengths 2, 3 and 5"
+  )
+})
