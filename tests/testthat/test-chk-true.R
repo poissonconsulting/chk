486a1449cd016bfa36a30b7eb10859bf2be3e863
Joe Thorley <joe@poissonconsulting.ca>
2025-01-03 12:27:05 -0800

    - Soft-deprecate `chk_lgl()` for `chk_scalar()` and `chk_logical()`.

--- tests/testthat/test-chk-true.R
+++ tests/testthat/test-chk-true.R
@@ -51,19 +51,3 @@ test_that("chk_flag", {
   expect_chk_error(chk_flag(1), "^`1` must be a flag [(]TRUE or FALSE[)][.]$")
   expect_chk_error(chk_flag(1, x_name = "2"), "^2 must be a flag [(]TRUE or FALSE[)][.]$")
 })
-
-test_that("vld_lgl", {
-  expect_true(vld_lgl(TRUE))
-  expect_true(vld_lgl(FALSE))
-  expect_false(vld_lgl(NA_real_))
-  expect_false(vld_lgl(logical(0)))
-  expect_false(vld_flag(c(FALSE, TRUE)))
-  expect_false(vld_lgl(1))
-})
-
-test_that("chk_lgl", {
-  expect_identical(chk_lgl(NA), NA)
-  expect_invisible(chk_lgl(NA))
-  expect_chk_error(chk_lgl(1), "^`1` must be a logical scalar [(]TRUE, FALSE or NA[)][.]$")
-  expect_chk_error(chk_lgl(1, x_name = 2), "^2 must be a logical scalar [(]TRUE, FALSE or NA[)][.]$")
-})
