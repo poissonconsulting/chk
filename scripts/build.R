6a6ef69ec6f1c65d7a6f22fd41658ec9ac427b94
Joe Thorley <joe@poissonconsulting.ca>
2026-05-21 06:05:08 -0700

    update build.R

--- scripts/build.R
+++ scripts/build.R
@@ -1,14 +1,25 @@
-roxygen2md::roxygen2md()
 styler::style_pkg(
   scope = "line_breaks",
   filetype = c("R", "Rmd")
 )
+
 lintr::lint_package()
 
-devtools::test()
+roxygen2md::roxygen2md()
 devtools::document()
+
+devtools::build_readme()
 
 pkgdown::build_home()
 pkgdown::build_site()
 
+browseURL("docs/index.html")
+
+devtools::test()
 devtools::check()
+
+rcmdcheck::rcmdcheck(
+  args = c("--no-manual", "--as-cran"),
+  build_args = "--resave-data=best",
+  error_on = "warning"
+)
